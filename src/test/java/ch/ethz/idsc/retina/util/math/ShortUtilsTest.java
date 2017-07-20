81a5e0185ba7e5926ca312de10af8bf4f36573ca
Jan Hakenberg <jan.hakenberg@gmail.com>
2017-07-20 15:43:23 +0200

    gps packet decoding

---
+++ src/test/java/ch/ethz/idsc/retina/util/math/ShortUtilsTest.java
@@ -0,0 +1,12 @@
+// code by jph
+package ch.ethz.idsc.retina.util.math;
+
+import junit.framework.TestCase;
+
+public class ShortUtilsTest extends TestCase {
+  public void testSimple() {
+    assertEquals(ShortUtils._24bit((short) 0xfff), -1);
+    assertEquals(ShortUtils._24bit((short) 0x7ff), 0x7ff);
+    assertEquals(ShortUtils._24bit((short) 0xf7ff), 0x7ff);
+  }
+}
