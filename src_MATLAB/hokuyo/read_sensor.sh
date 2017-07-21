22b3923db62c3584087bacb96e2e4c60450190bb
jelavice <edo.jelavic@gmail.com>
2017-07-21 15:42:45 +0200

    Small fix

--- src_MATLAB/hokuyo/read_sensor.sh
+++ src_MATLAB/hokuyo/read_sensor.sh
@@ -22,9 +22,10 @@ echo "Flushing the matOUTpipe" > matPIPEout & cat matPIPEout
 file=$HOME/Public/urg_provider
 if [ ! -e "$file" ]; then
     echo "Binary is not where it should be! Change the path manually."
-    mkfifo matPIPEout
 fi
 
+#run matlab in an other terminal
 gnome-terminal --command="matlab -nosplash -nodesktop -r read_sensor"
 
+# run the hokuyo driver and pipe it
 $HOME/Public/urg_provider > matPIPEin < matPIPEout
