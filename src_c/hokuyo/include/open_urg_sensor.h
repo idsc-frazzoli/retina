c685bec22a3280b6af7973dfb3609b4a398c74e4
jelavice <edo.jelavic@gmail.com>
2017-07-21 19:20:48 +0200

    Added cmake lists. Added mising code, referenced in the file urg_provider.c

---
+++ src_c/hokuyo/include/open_urg_sensor.h
@@ -0,0 +1,16 @@
+#ifndef OPEN_URG_SENSOR_H
+#define OPEN_URG_SENSOR_H
+
+/*!
+  \brief Connects to URG
+  \author Satofumi KAMIMURA
+
+  $Id: open_urg_sensor.h,v c5747add6615 2015/05/07 03:18:34 alexandr $
+*/
+
+#include "urg_sensor.h"
+
+
+extern int open_urg_sensor(urg_t *urg, int argc, char *argv[]);
+
+#endif /* !OPEN_URG_SENSOR_H */
