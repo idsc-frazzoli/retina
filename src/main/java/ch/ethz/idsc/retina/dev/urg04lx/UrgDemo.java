ae8dd40c71a04b4912bc0bba7723bb4c01009e96
Jan Hakenberg <jan.hakenberg@gmail.com>
2017-07-25 16:49:30 +0200

    urg demo corrections, thanks to @jelavice

--- src/main/java/ch/ethz/idsc/retina/dev/urg04lx/UrgDemo.java
+++ src/main/java/ch/ethz/idsc/retina/dev/urg04lx/UrgDemo.java
@@ -7,6 +7,7 @@ import java.awt.Graphics2D;
 import java.awt.Shape;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.awt.geom.Line2D;
 import java.awt.geom.Path2D;
 import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
@@ -22,27 +23,43 @@ import javax.swing.WindowConstants;
 
 import ch.ethz.idsc.retina.util.io.UserHome;
 import ch.ethz.idsc.tensor.RealScalar;
+import ch.ethz.idsc.tensor.Scalar;
 import ch.ethz.idsc.tensor.Scalars;
 import ch.ethz.idsc.tensor.Tensor;
 import ch.ethz.idsc.tensor.Tensors;
 import ch.ethz.idsc.tensor.alg.Subdivide;
 import ch.ethz.idsc.tensor.lie.RotationMatrix;
+import ch.ethz.idsc.tensor.red.Max;
+import ch.ethz.idsc.tensor.red.Min;
 
 /** {@link UrgDemo} requires that the binary "urg_provider" is located at
  * /home/{username}/Public/urg_provider
  * 
- * https://sourceforge.net/projects/urgnetwork/files/urg_library/ */
+ * https://sourceforge.net/projects/urgnetwork/files/urg_library/
+ * 
+ * Quote from datasheet:
+ * The light source of the sensor is infrared laser of
+ * wavelength 785nm with laser class 1 safety
+ * Max. Distance: 4000[mm]
+ * 
+ * The sensor is designed for indoor use only.
+ * The sensor is not a safety device/tool.
+ * The sensor is not for use in military applications.
+ * 
+ * typically the distances up to 5[m] can be measured correctly. */
 public class UrgDemo {
-  public static final double SCALE = 0.1;
+  public static final double SCALE = 0.15;
+  public static final Scalar THRESHOLD = RealScalar.of(30); // [mm]
   // ---
-  Tensor alpha = Subdivide.of(-170 * Math.PI / 180, 170 * Math.PI / 180, 681).unmodifiable();
+  /** p.2 Detection Area: 240 [deg] */
+  Tensor alpha = Subdivide.of(-120 * Math.PI / 180, 120 * Math.PI / 180, 681).unmodifiable();
   /** range contains distances in [mm] for 682 angles TODO confirm units */
   Tensor range = Tensors.empty();
 
   static Point2D toPoint(Tensor dir) {
     return new Point2D.Double( //
-        300 - dir.Get(1).number().doubleValue() * SCALE, //
-        300 - dir.Get(0).number().doubleValue() * SCALE);
+        700 + dir.Get(1).number().doubleValue() * SCALE, //
+        200 + dir.Get(0).number().doubleValue() * SCALE);
   }
 
   JFrame jFrame = new JFrame();
@@ -51,20 +68,31 @@ public class UrgDemo {
     protected void paintComponent(Graphics g) {
       Graphics2D graphics = (Graphics2D) g;
       {
-        graphics.setColor(new Color(128, 128, 128, 128));
         Path2D path2d = new Path2D.Double();
         {
           Point2D point2d = toPoint(Tensors.vector(0, 0));
           path2d.moveTo(point2d.getX(), point2d.getY());
         }
-        for (int index = 0; index < range.length(); ++index)
-          if (Scalars.nonZero(range.Get(index))) {
-            Tensor dir = RotationMatrix.of(alpha.Get(index)).get(Tensor.ALL, 0).multiply(range.Get(index));
-            Point2D p = toPoint(dir);
-            path2d.lineTo(p.getX(), p.getY());
-            Shape shape = new Rectangle2D.Double(p.getX(), p.getY(), 2, 2);
+        for (int index = 0; index < range.length(); ++index) {
+          final Tensor rotation = RotationMatrix.of(alpha.Get(index)).get(Tensor.ALL, 0);
+          if (Scalars.lessThan(range.Get(index), THRESHOLD)) {
+            graphics.setColor(new Color(30, 30, 30, 128));
+            Point2D p1 = toPoint(rotation.multiply(RealScalar.of(2000)));
+            Point2D p2 = toPoint(rotation.multiply(RealScalar.of(2050)));
+            graphics.draw(new Line2D.Double(p1.getX(), p1.getY(), p2.getX(), p2.getY()));
+          } else {
+            Tensor dir = rotation.multiply(range.Get(index));
+            final Point2D point = toPoint(dir);
+            path2d.lineTo(point.getX(), point.getY());
+            Shape shape = new Rectangle2D.Double(point.getX(), point.getY(), 2, 2);
+            graphics.setColor(new Color(128, 128, 128, 128));
             graphics.fill(shape);
+            if (index == range.length() / 2) {
+              graphics.setColor(Color.BLACK);
+              graphics.drawString("" + range.Get(index), (int) point.getX(), (int) point.getY());
+            }
           }
+        }
         graphics.setColor(new Color(128, 128 + 64, 128, 128));
         graphics.fill(path2d);
       }
@@ -82,12 +110,20 @@ public class UrgDemo {
         graphics.setColor(new Color(255, 128, 128, 128));
         graphics.fill(path2d);
       }
+      try {
+        Scalar min = range.flatten(-1).map(Scalar.class::cast).filter(Scalars::nonZero).reduce(Min::of).get();
+        Scalar max = range.flatten(-1).map(Scalar.class::cast).filter(Scalars::nonZero).reduce(Max::of).get();
+        graphics.setColor(Color.BLACK);
+        graphics.drawString("" + min + " " + max, 10, 10);
+      } catch (Exception exception) {
+        // ---
+      }
     }
   };
 
   public UrgDemo() {
     jFrame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
-    jFrame.setBounds(100, 100, 600, 600);
+    jFrame.setBounds(100, 100, 800, 800);
     jFrame.setContentPane(jComponent);
     jFrame.setVisible(true);
   }
