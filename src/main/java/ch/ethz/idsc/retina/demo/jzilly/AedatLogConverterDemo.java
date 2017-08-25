d52418cd8a94902a3a3b77235b7972c815d9e9c7
julian121266 <julianzilly@googlemail.com>
2017-08-25 15:50:27 +0200

    added logger to jzilly

---
+++ src/main/java/ch/ethz/idsc/retina/demo/jzilly/AedatLogConverterDemo.java
@@ -0,0 +1,19 @@
+// code by jph
+package ch.ethz.idsc.retina.demo.jzilly;
+
+import java.io.File;
+
+import ch.ethz.idsc.retina.dev.davis.app.AedatLogConverter;
+
+enum AedatLogConverterDemo {
+  ;
+  @SuppressWarnings("unused")
+  public static void main(String[] args) throws Exception {
+    // TODO configure
+    final File file1 = new File("/Users/julianzilly/Desktop/Projects/logged_data/", "DAVIS240C-2017-08-25T15-42-35+0200-02460010-0.aedat");
+    long tic = System.nanoTime();
+    AedatLogConverter.of(file1, new File("/Users/julianzilly/Desktop/Projects/logged_data/rec1/"));
+    long duration = System.nanoTime() - tic;
+    System.out.println((duration * 1e-9) + " [sec]");
+  }
+}
