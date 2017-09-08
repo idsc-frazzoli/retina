09b84ca24d39608c9f6e3b2b2dc1578a37b33ca1
Jan Hakenberg <jan.hakenberg@gmail.com>
2017-09-08 11:15:03 +0200

    snippet log

---
+++ src/main/java/ch/ethz/idsc/retina/demo/jph/davis/RunDavisSnippetLog.java
@@ -0,0 +1,17 @@
+// code by jph
+package ch.ethz.idsc.retina.demo.jph.davis;
+
+import java.io.File;
+
+import ch.ethz.idsc.retina.lcm.davis.DavisSnippetLog;
+
+enum RunDavisSnippetLog {
+  ;
+  public static void main(String[] args) {
+    // TODO change target directory
+    int period_ms = 2000;
+    File lcmDir = new File("/media/datahaki/media/ethz/snippet/lcm");
+    File uzhDir = new File("/media/datahaki/media/ethz/snippet/uzh");
+    new DavisSnippetLog(period_ms, lcmDir, uzhDir);
+  }
+}
