8483242353ea3706c4da5fd31ec9325f5f8235c2
Jan Hakenberg <jan.hakenberg@gmail.com>
2017-11-14 11:53:03 +0100

    pi rimo rate controller anti windup with @jelavice

--- src/main/java/ch/ethz/idsc/retina/dev/rimo/PIRimoRateController.java
+++ src/main/java/ch/ethz/idsc/retina/dev/rimo/PIRimoRateController.java
@@ -9,17 +9,16 @@ public class PIRimoRateController {
   private final Scalar dt = Quantity.of(RimoSocket.SEND_PERIOD_MS * 1e-3, "s");
   /** pos error initially incorrect in the first iteration */
   private Scalar lastVel_error = Quantity.of(0, RimoGetTire.UNIT_RATE);
-  private Scalar lastTor_value = Quantity.of(0, RimoGetTire.UNIT_RATE);
+  private Scalar lastTor_value = Quantity.of(0, RimoPutTire.UNIT_TORQUE);
 
   /** @param vel_error
    * @return */
-  public Scalar iterate(Scalar vel_error) { // "e"
+  public Scalar iterate(Scalar vel_error) {
     Scalar pPart = vel_error.subtract(lastVel_error).multiply(RimoConfig.GLOBAL.Kp);
     Scalar iPart = vel_error.multiply(RimoConfig.GLOBAL.Ki).multiply(dt);
     lastVel_error = vel_error;
     Scalar tor_value = lastTor_value.add(pPart).add(iPart);
-    lastTor_value = tor_value;
-    // FIXME anti-windup missing
-    return RimoConfig.GLOBAL.torqueLimitClip().apply(tor_value);
+    lastTor_value = RimoConfig.GLOBAL.torqueLimitClip().apply(tor_value); // anti-windup
+    return lastTor_value;
   }
 }
