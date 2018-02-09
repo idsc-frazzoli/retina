7fe184e7f7ed98f4133c6ef8981f9cb440db5f28
Jan Hakenberg <jan.hakenberg@gmail.com>
2018-02-09 11:44:58 +0100

    slowdown tests

--- src/test/java/ch/ethz/idsc/retina/dev/rimo/RimoRateControllerUnoTest.java
+++ src/test/java/ch/ethz/idsc/retina/dev/rimo/RimoRateControllerUnoTest.java
@@ -5,6 +5,7 @@ import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 
 import ch.ethz.idsc.tensor.RealScalar;
+import ch.ethz.idsc.tensor.Tensors;
 import ch.ethz.idsc.tensor.qty.Quantity;
 import junit.framework.TestCase;
 
@@ -38,5 +39,24 @@ public class RimoRateControllerUnoTest extends TestCase {
     assertEquals(rimoPutEvent.putL.getTorqueRaw(), -rimoPutEvent.putR.getTorqueRaw());
     assertTrue(rimoPutEvent.putL.getTorqueRaw() < 0);
     assertTrue(rimoPutEvent.putR.getTorqueRaw() > 0);
+  }
+
+  public void testSlowdown() {
+    RimoRateControllerWrap rrcw = new RimoRateControllerUno();
+    assertFalse(rrcw.iterate(Quantity.of(1, "rad*s^-1"), RealScalar.of(0.1)).isPresent());
+    ByteBuffer byteBuffer = ByteBuffer.wrap(new byte[48]);
+    byteBuffer.order(ByteOrder.LITTLE_ENDIAN);
+    byteBuffer.putShort(2, (short) -126);
+    byteBuffer.putShort(2 + 24, (short) 120);
+    byteBuffer.position(0);
+    RimoGetEvent rimoGetEvent = RimoSocket.INSTANCE.createGetEvent(byteBuffer);
+    assertEquals(rimoGetEvent.getAngularRate_Y_pair(), Tensors.fromString("{2.1[rad*s^-1], 2.0[rad*s^-1]}"));
+    rrcw.getEvent(rimoGetEvent);
+    // RimoPutEvent rimoPutEvent = rrcw.iterate(Tensors.fromString("{1[rad*s^-1],2[rad*s^-1]}")).get();
+    RimoPutEvent rimoPutEvent = rrcw.iterate(Quantity.of(1, "rad*s^-1"), RealScalar.of(0.1)).get();
+    // because "uno" uses a single PI controller, the torques have the same absolute value
+    assertEquals(rimoPutEvent.putL.getTorqueRaw(), -rimoPutEvent.putR.getTorqueRaw());
+    assertTrue(rimoPutEvent.putL.getTorqueRaw() > 0);
+    assertTrue(rimoPutEvent.putR.getTorqueRaw() < 0);
   }
 }
