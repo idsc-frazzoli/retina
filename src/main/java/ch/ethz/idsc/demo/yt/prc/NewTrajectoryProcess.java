5c2a2f8fe891b943b091f8622ee644b0a05d1052
Jan Hakenberg <jan.hakenberg@gmail.com>
2018-03-06 15:09:05 +0100

    inspecting all dvs events in a log file

--- src/main/java/ch/ethz/idsc/demo/yt/prc/NewTrajectoryProcess.java
+++ src/main/java/ch/ethz/idsc/demo/yt/prc/NewTrajectoryProcess.java
@@ -5,25 +5,40 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 
-import ch.ethz.idsc.gokart.gui.GokartStatusEvent;
 import ch.ethz.idsc.owl.bot.util.UserHome;
-import ch.ethz.idsc.retina.dev.steer.SteerColumnInterface;
+import ch.ethz.idsc.retina.dev.davis.DavisDvsListener;
+import ch.ethz.idsc.retina.dev.davis._240c.DavisDvsEvent;
+import ch.ethz.idsc.retina.dev.davis.data.DavisDvsDatagramDecoder;
 import ch.ethz.idsc.retina.lcm.OfflineLogListener;
 import ch.ethz.idsc.retina.lcm.OfflineLogPlayer;
 import ch.ethz.idsc.tensor.Scalar;
 
-public class NewTrajectoryProcess implements OfflineLogListener {
+/** class adapted for gioele
+ * extract all dvs events */
+class NewTrajectoryProcess implements OfflineLogListener, DavisDvsListener {
+  private final DavisDvsDatagramDecoder davisDvsDatagramDecoder = new DavisDvsDatagramDecoder();
+
+  public NewTrajectoryProcess() {
+    davisDvsDatagramDecoder.addDvsListener(this);
+  }
+
   @Override
   public void event(Scalar time, String channel, ByteBuffer byteBuffer) {
-    if (channel.equals("gokart.status.get")) {
-      SteerColumnInterface gse = new GokartStatusEvent(byteBuffer);
-      System.out.println(time + " " + gse.getSteerColumnEncoderCentered());
+    System.out.println(channel);
+    if (channel.equals("davis240c.overview.dvs")) {
+      davisDvsDatagramDecoder.decode(byteBuffer);
     }
   }
 
+  @Override
+  public void davisDvs(DavisDvsEvent davisDvsEvent) {
+    System.out.println(davisDvsEvent.toString());
+  }
+
   public static void main(String[] args) throws IOException {
-    File file = UserHome.file("gokart/pursuit/20180305T170018/log.lcm");
-    OfflineLogListener oll = new NewTrajectoryProcess();
+    File file = UserHome.file("gokart/twist/20180108T165210_4/log.lcm");
+    NewTrajectoryProcess oll = new NewTrajectoryProcess();
+    oll.davisDvsDatagramDecoder.addDvsListener(oll);
     OfflineLogPlayer.process(file, oll);
   }
 }
