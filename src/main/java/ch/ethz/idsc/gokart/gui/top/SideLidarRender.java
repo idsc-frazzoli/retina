291d0fa0d4014b166eb58aef8a5d2d25e4079f74
Jan Hakenberg <jan.hakenberg@gmail.com>
2018-03-10 20:49:01 +0100

    lidar reference side view

--- src/main/java/ch/ethz/idsc/gokart/gui/top/SideLidarRender.java
+++ src/main/java/ch/ethz/idsc/gokart/gui/top/SideLidarRender.java
@@ -10,6 +10,8 @@ import java.util.Objects;
 import ch.ethz.idsc.gokart.core.pos.GokartPoseInterface;
 import ch.ethz.idsc.owl.gui.win.GeometricLayer;
 import ch.ethz.idsc.owl.math.map.Se2Utils;
+import ch.ethz.idsc.retina.util.math.Magnitude;
+import ch.ethz.idsc.tensor.RealScalar;
 import ch.ethz.idsc.tensor.Tensor;
 import ch.ethz.idsc.tensor.Tensors;
 
@@ -21,6 +23,12 @@ class SideLidarRender extends LidarRender {
   @Override // from AbstractGokartRender
   public void protected_render(GeometricLayer geometricLayer, Graphics2D graphics) {
     geometricLayer.pushMatrix(Se2Utils.toSE2Matrix(supplier.get()));
+    Tensor translate = Se2Utils.toSE2Matrix(Tensors.of( //
+        SensorsConfig.GLOBAL.vlp16.Get(0), // translation right (in pixel space)
+        Magnitude.METER.apply(SensorsConfig.GLOBAL.vlp16Height), // translation up (in pixel space) to
+        RealScalar.ZERO // rotation is pixel space
+    ));
+    geometricLayer.pushMatrix(translate);
     {
       Point2D point2D = geometricLayer.toPoint2D(Tensors.vector(0, 0));
       Point2D width = geometricLayer.toPoint2D(Tensors.vector(0.1, 0));
@@ -30,12 +38,6 @@ class SideLidarRender extends LidarRender {
     }
     if (Objects.nonNull(_points)) {
       Tensor points = _points;
-      Tensor translate = Se2Utils.toSE2Matrix(Tensors.vector( //
-          0, // translation right (in pixel space)
-          0, // translation up (in pixel space) TODO VC use SensorsConfig.GLOBAL.vlp16Height to
-          0 // rotation is pixel space
-      ));
-      geometricLayer.pushMatrix(translate);
       // ---
       graphics.setColor(color);
       for (Tensor x : points) {
@@ -48,8 +50,8 @@ class SideLidarRender extends LidarRender {
         graphics.fillRect((int) point2D.getX(), (int) point2D.getY(), pointSize, pointSize);
       }
       // ---
-      geometricLayer.popMatrix();
     }
+    geometricLayer.popMatrix();
     geometricLayer.popMatrix();
   }
 }
