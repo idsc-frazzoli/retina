a2c5bb31dab45e4ef42200365209b0402f70ff85
Gioele Zardini <gzardini@student.ethz.ch>
2018-03-19 14:42:34 +0100

    added some conversion files in order to analyze complete sets of .csv files

---
+++ src/main/java/ch/ethz/idsc/demo/gz/DavisLcmLogUzhConverterBatch.java
@@ -0,0 +1,23 @@
+// code by az
+package ch.ethz.idsc.demo.gz;
+
+import java.io.File;
+
+import ch.ethz.idsc.retina.lcm.davis.DavisLcmLogUzhConvert;
+import ch.ethz.idsc.subare.util.UserHome;
+
+enum DavisLcmLogUzhConverterBatch {
+  ;
+  public static void process(File file, File destination) {
+    DavisLcmLogUzhConvert.of(file, destination);
+  }
+
+  public static void main(String[] args) {
+	  File dir = new File("/home/gio/Downloads/logs/");
+	  File destination = new File("/home/gio/Downloads/logs/");
+    for (File file : dir.listFiles()) {
+      System.out.println(file);
+      process(file, destination);
+    }
+  }
+}
