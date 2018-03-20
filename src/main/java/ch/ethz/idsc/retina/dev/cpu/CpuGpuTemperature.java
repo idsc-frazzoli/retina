0364bcc56a900ccc041961a3db8acea6693ddb4e
alezana <azanardi@ethz.ch>
2018-03-20 18:02:27 +0100

    gpu not working yet

--- src/main/java/ch/ethz/idsc/retina/dev/cpu/CpuGpuTemperature.java
+++ src/main/java/ch/ethz/idsc/retina/dev/cpu/CpuGpuTemperature.java
@@ -5,14 +5,16 @@ import java.util.List;
 import com.profesorfalken.jsensors.JSensors;
 import com.profesorfalken.jsensors.model.components.Components;
 import com.profesorfalken.jsensors.model.components.Cpu;
+import com.profesorfalken.jsensors.model.components.Gpu;
 import com.profesorfalken.jsensors.model.sensors.Fan;
 import com.profesorfalken.jsensors.model.sensors.Temperature;
 
-enum CpuTemperature {
+enum CpuGpuTemperature {
   ;
   public static void main(String[] args) {
     Components components = JSensors.get.components();
     List<Cpu> cpus = components.cpus;
+    List<Gpu> gpus = components.gpus;
     if (cpus != null) {
       for (final Cpu cpu : cpus) {
         System.out.println("Found CPU component: " + cpu.name);
@@ -25,6 +27,22 @@ enum CpuTemperature {
           }
           // Print fan speed
           List<Fan> fans = cpu.sensors.fans;
+          for (final Fan fan : fans) {
+            System.out.println(fan.name + ": " + fan.value + " RPM");
+          }
+        }
+      }
+      for (final Gpu gpu : gpus) {
+        System.out.println("Found GPU component: " + gpu.name);
+        if (gpu.sensors != null) {
+          System.out.println("Sensors: ");
+          // Print temperatures
+          List<Temperature> temps = gpu.sensors.temperatures;
+          for (final Temperature temp : temps) {
+            System.out.println(temp.name + ": " + temp.value + " C");
+          }
+          // Print fan speed
+          List<Fan> fans = gpu.sensors.fans;
           for (final Fan fan : fans) {
             System.out.println(fan.name + ": " + fan.value + " RPM");
           }
