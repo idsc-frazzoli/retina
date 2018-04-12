3c5d9412960687692e54eb438b89e9eda7389ffe
Jan Hakenberg <jan.hakenberg@gmail.com>
2018-04-12 22:51:37 +0200

    gokart messages

--- src/main/java/ch/ethz/idsc/retina/dev/joystick/JoystickDecoder.java
+++ src/main/java/ch/ethz/idsc/retina/dev/joystick/JoystickDecoder.java
@@ -6,8 +6,7 @@ import java.nio.ByteBuffer;
 public enum JoystickDecoder {
   ;
   public static JoystickEvent decode(ByteBuffer byteBuffer) {
-    int ordinal = byteBuffer.get() & 0xff;
-    // System.out.println(ordinal);
+    int ordinal = byteBuffer.get() & 0xff; // joystick device id
     JoystickEvent joystickEvent = JoystickType.values()[ordinal].supplier.get();
     joystickEvent.decode(byteBuffer);
     return joystickEvent;
