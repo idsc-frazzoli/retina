6d4cf16d238395800a8c037ee59841ca2817615e
Jan Hakenberg <jan.hakenberg@gmail.com>
2018-04-15 16:08:18 +0200

    min max of temperature array

--- src/main/java/ch/ethz/idsc/gokart/core/fuse/ComputerSensorsEvent.java
+++ src/main/java/ch/ethz/idsc/gokart/core/fuse/ComputerSensorsEvent.java
@@ -1,11 +1,8 @@
 // code by jph & az
 package ch.ethz.idsc.gokart.core.fuse;
 
-import java.awt.List;
 import java.nio.ByteBuffer;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Comparator;
+import java.util.stream.IntStream;
 
 import ch.ethz.idsc.retina.util.math.SI;
 import ch.ethz.idsc.tensor.Scalar;
@@ -14,22 +11,18 @@ import ch.ethz.idsc.tensor.qty.Quantity;
 public class ComputerSensorsEvent {
   private final int maxCpuTempCelsius;
   private final int minCpuTempCelsius;
-  private Integer[] cpus_temperatures;
 
   public ComputerSensorsEvent(ByteBuffer byteBuffer) {
-    byte protocol_id = byteBuffer.get(); // value is ignored until 2nd protocol emerges
+    // byte protocol_id =
+    byteBuffer.get(); // value is ignored until 2nd protocol emerges
     byte cpu_size = byteBuffer.get();
-    byte gpu_size = byteBuffer.get();
+    // byte gpu_size =
+    byteBuffer.get(); // zero for the gokart pc
     byte temps_size = byteBuffer.get();
-    // TODO read temps_size x cpu size number of bytes...
-    // ... and compute maximum maxCpuTempCelsius
-    for (int i = 0; i < (int) cpu_size; i++) {
-      for (int j = 0; j < (int) temps_size; j++) {
-        cpus_temperatures[j] = (int) byteBuffer.get();
-      }
-    }
-    maxCpuTempCelsius = Collections.max(cpus_temperatures, Comparator.nullsFirst(Comparator.naturalOrder())); // FIXME
-    minCpuTempCelsius = Collections.min(cpus_temperatures, Comparator.nullsFirst(Comparator.naturalOrder())); // FIXME
+    byte[] data = new byte[cpu_size * temps_size]; // allocate byte array
+    byteBuffer.get(data); // read bytes from buffer into array
+    maxCpuTempCelsius = IntStream.range(0, data.length).map(i -> data[i]).boxed().reduce(Math::max).get();
+    minCpuTempCelsius = IntStream.range(0, data.length).map(i -> data[i]).boxed().reduce(Math::min).get();
   }
 
   /** @return {@link Quantity} */
