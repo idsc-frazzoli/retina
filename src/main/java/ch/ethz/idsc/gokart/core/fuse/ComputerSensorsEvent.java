24a4cfa79dd378dde006842ba6596388394db4b3
alezana <azanardi@ethz.ch>
2018-04-15 10:49:36 +0200

    first raw attempt to read out temperature

--- src/main/java/ch/ethz/idsc/gokart/core/fuse/ComputerSensorsEvent.java
+++ src/main/java/ch/ethz/idsc/gokart/core/fuse/ComputerSensorsEvent.java
@@ -1,14 +1,20 @@
-// code by jph
+// code by jph & az
 package ch.ethz.idsc.gokart.core.fuse;
 
+import java.awt.List;
 import java.nio.ByteBuffer;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
 
 import ch.ethz.idsc.retina.util.math.SI;
 import ch.ethz.idsc.tensor.Scalar;
 import ch.ethz.idsc.tensor.qty.Quantity;
 
 public class ComputerSensorsEvent {
-  private final byte maxCpuTempCelsius;
+  private final int maxCpuTempCelsius;
+  private final int minCpuTempCelsius;
+  private Integer[] cpus_temperatures;
 
   public ComputerSensorsEvent(ByteBuffer byteBuffer) {
     byte protocol_id = byteBuffer.get(); // value is ignored until 2nd protocol emerges
@@ -17,11 +23,22 @@ public class ComputerSensorsEvent {
     byte temps_size = byteBuffer.get();
     // TODO read temps_size x cpu size number of bytes...
     // ... and compute maximum maxCpuTempCelsius
-    maxCpuTempCelsius = 127; // FIXME
+    for (int i = 0; i < (int) cpu_size; i++) {
+      for (int j = 0; j < (int) temps_size; j++) {
+        cpus_temperatures[j] = (int) byteBuffer.get();
+      }
+    }
+    maxCpuTempCelsius = Collections.max(cpus_temperatures, Comparator.nullsFirst(Comparator.naturalOrder())); // FIXME
+    minCpuTempCelsius = Collections.min(cpus_temperatures, Comparator.nullsFirst(Comparator.naturalOrder())); // FIXME
   }
 
   /** @return {@link Quantity} */
   public Scalar getTemperatureMax() {
     return Quantity.of(maxCpuTempCelsius, SI.DEGREE_CELSIUS);
+  }
+
+  /** @return {@link Quantity} */
+  public Scalar getTemperatureMin() {
+    return Quantity.of(minCpuTempCelsius, SI.DEGREE_CELSIUS);
   }
 }
--- src/test/java/ch/ethz/idsc/gokart/core/fuse/ComputerSensorsEventTest.java
+++ src/test/java/ch/ethz/idsc/gokart/core/fuse/ComputerSensorsEventTest.java
@@ -3,6 +3,8 @@ package ch.ethz.idsc.gokart.core.fuse;
 
 import java.nio.ByteBuffer;
 
+import ch.ethz.idsc.retina.util.math.SI;
+import ch.ethz.idsc.tensor.qty.Quantity;
 import junit.framework.TestCase;
 
 public class ComputerSensorsEventTest extends TestCase {
@@ -10,6 +12,7 @@ public class ComputerSensorsEventTest extends TestCase {
     byte[] array = new byte[] { 0, 1, 0, 3, 60, 67, 64 };
     ComputerSensorsEvent cse = new ComputerSensorsEvent(ByteBuffer.wrap(array));
     // TODO
-    // assertEquals(cse.getTemperatureMax(), Quantity.of(67, SI.DEGREE_CELSIUS));
+    assertEquals(cse.getTemperatureMax(), Quantity.of(67, SI.DEGREE_CELSIUS));
+    assertEquals(cse.getTemperatureMin(), Quantity.of(60, SI.DEGREE_CELSIUS));
   }
 }
