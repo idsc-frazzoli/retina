f80284e95e0213a2850dd419bbec63bf75a3d94d
jelavice <gokart@alienware.com>
2018-04-19 17:18:39 +0200

    74 11

--- src/main/java/ch/ethz/idsc/demo/jph/RimoRateAnalysis.java
+++ src/main/java/ch/ethz/idsc/demo/jph/RimoRateAnalysis.java
@@ -4,7 +4,7 @@ import java.io.File;
 import java.io.IOException;
 
 import ch.ethz.idsc.demo.DubendorfHangarLog;
-import ch.ethz.idsc.gokart.offline.tab.RimoRateTable;
+import ch.ethz.idsc.gokart.offline.tab.RimoRateJoystickTable;
 import ch.ethz.idsc.owl.bot.util.UserHome;
 import ch.ethz.idsc.retina.lcm.OfflineLogPlayer;
 import ch.ethz.idsc.tensor.io.CsvFormat;
@@ -21,9 +21,9 @@ enum RimoRateAnalysis {
     // _20180307T154859_0cd18c6b no extraction needed (with stops)
     File file = DubendorfHangarLog._20180307T154859_0cd18c6b.file(LOG_ROOT);
     file = UserHome.file("gokart/pursuit/20180307T154859/log.lcm");
-    file = UserHome.file("20180418T132333_bca165ae_prbs4.lcm");
-    RimoRateTable rimoRateTable = new RimoRateTable(Quantity.of(0.01, "s"));
+    file = UserHome.file("gokartlogs/20180419/20180419T150253_7373f83e.lcm.00");
+    RimoRateJoystickTable rimoRateTable = new RimoRateJoystickTable(Quantity.of(0.01, "s"));
     OfflineLogPlayer.process(file, rimoRateTable);
-    Export.of(UserHome.file("20180418T132333_bca165ae_prbs4.csv"), rimoRateTable.getTable().map(CsvFormat.strict()));
+    Export.of(UserHome.file("20180419T150253_7373f83e_joy.csv"), rimoRateTable.getTable().map(CsvFormat.strict()));
   }
 }
