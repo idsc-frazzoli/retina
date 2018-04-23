f41fb5116d1c466a9fd410183014d60f144db4d3
Jan Hakenberg <jan.hakenberg@gmail.com>
2018-04-23 22:44:09 +0200

    log file, offline tables tests

---
+++ src/test/java/ch/ethz/idsc/gokart/offline/tab/DavisImuTableTest.java
@@ -0,0 +1,26 @@
+// code by jph
+package ch.ethz.idsc.gokart.offline.tab;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+import ch.ethz.idsc.gokart.offline.api.GokartLogAdapterTest;
+import ch.ethz.idsc.gokart.offline.api.GokartLogInterface;
+import ch.ethz.idsc.gokart.offline.api.OfflineTableSupplier;
+import ch.ethz.idsc.retina.lcm.OfflineLogPlayer;
+import ch.ethz.idsc.tensor.Tensor;
+import ch.ethz.idsc.tensor.alg.Dimensions;
+import ch.ethz.idsc.tensor.io.CsvFormat;
+import ch.ethz.idsc.tensor.qty.Quantity;
+import junit.framework.TestCase;
+
+public class DavisImuTableTest extends TestCase {
+  public void testSimple() throws IOException {
+    GokartLogInterface gokartLogInterface = GokartLogAdapterTest.FULL;
+    // ---
+    OfflineTableSupplier offlineTableSupplier = new DavisImuTable(Quantity.of(0, "s"));
+    OfflineLogPlayer.process(gokartLogInterface.file(), offlineTableSupplier);
+    Tensor tensor = offlineTableSupplier.getTable().map(CsvFormat.strict());
+    assertEquals(Dimensions.of(tensor), Arrays.asList(719, 11));
+  }
+}
---
+++ src/test/java/ch/ethz/idsc/gokart/offline/tab/PowerSteerTableTest.java
@@ -0,0 +1,27 @@
+// code by jph
+package ch.ethz.idsc.gokart.offline.tab;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+import ch.ethz.idsc.gokart.offline.api.GokartLogAdapterTest;
+import ch.ethz.idsc.gokart.offline.api.GokartLogInterface;
+import ch.ethz.idsc.gokart.offline.api.OfflineTableSupplier;
+import ch.ethz.idsc.retina.lcm.OfflineLogPlayer;
+import ch.ethz.idsc.tensor.Tensor;
+import ch.ethz.idsc.tensor.alg.Dimensions;
+import ch.ethz.idsc.tensor.io.CsvFormat;
+import ch.ethz.idsc.tensor.qty.Quantity;
+import junit.framework.TestCase;
+
+public class PowerSteerTableTest extends TestCase {
+  public void testSimple() throws IOException {
+    GokartLogInterface gokartLogInterface = GokartLogAdapterTest.FULL;
+    // ---
+    OfflineTableSupplier offlineTableSupplier = new PowerSteerTable(Quantity.of(0, "s"));
+    OfflineLogPlayer.process(gokartLogInterface.file(), offlineTableSupplier);
+    Tensor tensor = offlineTableSupplier.getTable().map(CsvFormat.strict());
+    // System.out.println(Dimensions.of(tensor));
+    assertEquals(Dimensions.of(tensor), Arrays.asList(6052, 15));
+  }
+}
