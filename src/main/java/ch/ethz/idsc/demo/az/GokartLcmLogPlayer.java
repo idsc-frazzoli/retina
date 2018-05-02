2a0e3aeae8147d63cb60743b78e6a0c6465fe1d0
alezana <azanardi@ethz.ch>
2018-05-02 17:59:37 +0200

    extract csv az

--- src/main/java/ch/ethz/idsc/demo/az/GokartLcmLogPlayer.java
+++ src/main/java/ch/ethz/idsc/demo/az/GokartLcmLogPlayer.java
@@ -15,7 +15,9 @@ enum GokartLcmLogPlayer {
   public static void main(String[] args) throws IOException {
     LogPlayerConfig cfg = new LogPlayerConfig();
     File file;
-    file = UserHome.file("/datasets/gokart_logs/20180412T152900_7e5b46c2.lcm.00");
+    file = UserHome.file("datasets/gokart_logs/20180412T152900_7e5b46c2.lcm.00");
+    file = UserHome.file("datasets/gokart_logs/20180423T181849_633cc6e6.lcm.00");
+    System.out.println(file.isFile());
     cfg.logFile = file.toString();
     cfg.speed_numerator = 1;
     cfg.speed_denominator = 3;
