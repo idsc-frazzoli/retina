ba1996e62a8fbf4ee9fd8de84c6c2ad59bb6ca83
ynager <ynager@student.ethz.ch>
2018-05-07 00:30:09 +0200

    Analysis of planner lcm logs

---
+++ src/main/java/ch/ethz/idsc/demo/yn/PlannerAnalysis.java
@@ -0,0 +1,36 @@
+// code by ynager
+package ch.ethz.idsc.demo.yn;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import ch.ethz.idsc.gokart.core.pos.LocalizationConfig;
+import ch.ethz.idsc.gokart.gui.top.PredefinedMap;
+import ch.ethz.idsc.gokart.offline.api.GokartLogAdapter;
+import ch.ethz.idsc.gokart.offline.api.GokartLogInterface;
+import ch.ethz.idsc.gokart.offline.api.OfflineTableSupplier;
+import ch.ethz.idsc.gokart.offline.tab.TrajectoryTable;
+import ch.ethz.idsc.retina.lcm.OfflineLogPlayer;
+import ch.ethz.idsc.subare.util.UserHome;
+import ch.ethz.idsc.tensor.io.CsvFormat;
+import ch.ethz.idsc.tensor.io.Export;
+
+enum PlannerAnalysis {
+  ;
+  public static void main(String[] args) throws FileNotFoundException, IOException {
+    PredefinedMap predefinedMap = LocalizationConfig.getPredefinedMap();
+    File file = UserHome.file("gokart/logs");
+    System.out.println(file.getName());
+    GokartLogInterface olr = GokartLogAdapter.of(file);
+    // ---
+    // ScatterImage scatterImage = new PoseScatterImage(predefinedMap);
+    // scatterImage = new WallScatterImage(predefinedMap);
+    // ---
+    OfflineTableSupplier offlineTableSupplier = new TrajectoryTable();
+    OfflineLogPlayer.process(olr.file(), offlineTableSupplier);
+    Export.of(UserHome.file(file.getName() + "_planer.csv"), offlineTableSupplier.getTable().map(CsvFormat.strict()));
+    // ImageIO.write(scatterImage.getImage(), "png", UserHome.Pictures(file.getName() + ".png"));
+    System.out.print("done.");
+  }
+}
--- src/main/java/ch/ethz/idsc/gokart/lcm/mod/PlannerPublish.java
+++ src/main/java/ch/ethz/idsc/gokart/lcm/mod/PlannerPublish.java
@@ -17,25 +17,9 @@ public enum PlannerPublish {
   ;
   // TODO encoding not final: node info may be sufficient, flow not considered yet
   public static void publishTrajectory(List<TrajectorySample> trajectory) {
-    // if (!trajectory.isEmpty())
     {
       Tensor tensor = Tensor.of(trajectory.stream().map(ts -> ts.stateTime().joined()));
       BinaryBlob binaryBlob = ArrayFloatBlob.encode(tensor);
-      // publish trajectory states & times
-      // BinaryBlob binaryBlob = new BinaryBlob();
-      // final int stateDim = trajectory.get(0).stateTime().state().length();
-      // final int dataLength = trajectory.size() * (stateDim + 1) * Float.BYTES;
-      // System.out.print(trajectory.size());
-      // binaryBlob.data = new byte[dataLength];
-      // binaryBlob.data_length = dataLength;
-      // ByteBuffer byteBuffer = ByteBuffer.wrap(binaryBlob.data);
-      // byteBuffer.order(ByteOrder.LITTLE_ENDIAN);
-      // for (TrajectorySample ts : trajectory) {
-      // float[] data = Primitives.toFloatArray(ts.stateTime().state());
-      // for (float value : data)
-      // byteBuffer.putFloat(value);
-      // byteBuffer.putFloat(ts.stateTime().time().number().floatValue());
-      // }
       LCM.getSingleton().publish(GokartLcmChannel.TRAJECTORY_STATETIME, binaryBlob);
     }
   }
