86a3a7777e400a4cd1ba6ae0d51f2e9d492fd249
Jan Hakenberg <jan.hakenberg@gmail.com>
2018-05-07 06:01:02 +0200

    localization in separate thread

--- src/main/java/ch/ethz/idsc/gokart/core/slam/LidarLocalizationModule.java
+++ src/main/java/ch/ethz/idsc/gokart/core/slam/LidarLocalizationModule.java
@@ -2,6 +2,7 @@
 package ch.ethz.idsc.gokart.core.slam;
 
 import java.nio.FloatBuffer;
+import java.util.Objects;
 import java.util.Optional;
 
 import ch.ethz.idsc.gokart.core.pos.GokartPoseLcmServer;
@@ -22,7 +23,7 @@ import ch.ethz.idsc.tensor.RealScalar;
 import ch.ethz.idsc.tensor.Tensor;
 import ch.ethz.idsc.tensor.Tensors;
 
-public class LidarLocalizationModule extends AbstractModule implements LidarRayBlockListener {
+public class LidarLocalizationModule extends AbstractModule implements LidarRayBlockListener, Runnable {
   // TODO bad design
   public static boolean TRACKING = false;
   public static boolean FLAGSNAP = false;
@@ -48,39 +49,60 @@ public class LidarLocalizationModule extends AbstractModule implements LidarRayB
     // ---
     vlp16LcmHandler.startSubscriptions();
     davisImuLcmClient.startSubscriptions();
+    thread.start();
   }
 
   @Override // from AbstractModule
   protected void last() {
+    isLaunched = false;
+    thread.interrupt();
     vlp16LcmHandler.stopSubscriptions();
     davisImuLcmClient.stopSubscriptions();
   }
 
+  private final Thread thread = new Thread(this);
+  private Tensor points_ferry = null;
+
   @Override
   public void lidarRayBlock(LidarRayBlockEvent lidarRayBlockEvent) { // receive 2D block event
-    FloatBuffer floatBuffer = lidarRayBlockEvent.floatBuffer;
-    Tensor points = Tensors.vector(i -> Tensors.of( //
-        DoubleScalar.of(floatBuffer.get()), //
-        DoubleScalar.of(floatBuffer.get())), lidarRayBlockEvent.size());
-    // Optional<SlamResult> optional = lidarGyroLocalization.handle(points);
-    Tensor state = gokartPoseOdometry.getPose(); // {x[m],y[m],angle[]}
-    // System.out.println("HERE " + Dimensions.of(points) + " " + state);
-    // System.out.println("IN = " + state);
     if (FLAGSNAP || TRACKING) {
       FLAGSNAP = false;
-      // System.out.println("tracking");
-      lidarGyroLocalization.setState(state);
-      // Stopwatch stopwatch = Stopwatch.started();
-      Optional<SlamResult> optional = lidarGyroLocalization.handle(points);
-      // double duration = stopwatch.display_seconds();
-      if (optional.isPresent()) {
-        SlamResult slamResult = optional.get();
-        // OUT={37.85[m], 38.89[m], -0.5658221}
-        gokartPoseOdometry.setPose(slamResult.getTransform(), slamResult.getMatchRatio());
-      } else {
-        // System.err.println("insufficient: " + sum);
-        gokartPoseOdometry.setPose(state, RealScalar.ZERO);
-      }
+      FloatBuffer floatBuffer = lidarRayBlockEvent.floatBuffer;
+      points_ferry = Tensors.vector(i -> Tensors.of( //
+          DoubleScalar.of(floatBuffer.get()), //
+          DoubleScalar.of(floatBuffer.get())), lidarRayBlockEvent.size());
+      thread.interrupt();
     }
+  }
+
+  private boolean isLaunched = true;
+
+  @Override
+  public void run() {
+    while (isLaunched) {
+      Tensor points = points_ferry;
+      if (Objects.nonNull(points)) {
+        points_ferry = null;
+        Tensor state = gokartPoseOdometry.getPose(); // {x[m],y[m],angle[]}
+        // System.out.println("tracking");
+        lidarGyroLocalization.setState(state);
+        // Stopwatch stopwatch = Stopwatch.started();
+        Optional<SlamResult> optional = lidarGyroLocalization.handle(points);
+        // double duration = stopwatch.display_seconds();
+        if (optional.isPresent()) {
+          SlamResult slamResult = optional.get();
+          // OUT={37.85[m], 38.89[m], -0.5658221}
+          gokartPoseOdometry.setPose(slamResult.getTransform(), slamResult.getMatchRatio());
+        } else
+          // TODO check is the code below is sufficient
+          gokartPoseOdometry.setPose(state, RealScalar.ZERO);
+      } else
+        try {
+          Thread.sleep(1000);
+        } catch (InterruptedException e) {
+          // ---
+        }
+    }
+    System.out.println("exit thread");
   }
 }
