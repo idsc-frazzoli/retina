e2e97ba533750c3eacdf79fc1a7669fb0cfd8e41
ValentinaCavinato <vcavinat@student.ethz.ch>
2018-05-18 00:07:21 +0200

    performance evaluation, enlarged points

---
+++ src/main/java/ch/ethz/idsc/demo/vc/EnlargedPoints.java
@@ -0,0 +1,23 @@
+package ch.ethz.idsc.demo.vc;
+
+import java.awt.geom.Area;
+import java.awt.geom.Ellipse2D;
+import java.util.ArrayList;
+import java.util.List;
+
+import ch.ethz.idsc.tensor.Tensor;
+
+public class EnlargedPoints {
+  public List<Area> collectionOfAreas = new ArrayList<>(); // TODO
+  private int w = 2; // TODO
+
+  public EnlargedPoints(Tensor points) {
+    for (Tensor x : points) {
+      addToCollection(x);
+    }
+  }
+
+  public void addToCollection(Tensor point) {
+    collectionOfAreas.add(new Area(new Ellipse2D.Double(point.Get(0).number().doubleValue() - w / 2, point.Get(1).number().doubleValue() - w / 2, w, w)));
+  }
+}
