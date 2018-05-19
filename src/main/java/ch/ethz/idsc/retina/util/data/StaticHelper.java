73b038742d58e0ec5324aea777df908e801e8e6a
Jan Hakenberg <jan.hakenberg@gmail.com>
2018-05-19 21:16:05 +0200

    parameters component

--- src/main/java/ch/ethz/idsc/retina/util/data/StaticHelper.java
+++ src/main/java/ch/ethz/idsc/retina/util/data/StaticHelper.java
@@ -8,21 +8,6 @@ import java.util.Properties;
 
 enum StaticHelper {
   ;
-  /** "true" */
-  private static final String TRUE = Boolean.TRUE.toString();
-  /** "false" */
-  private static final String FALSE = Boolean.FALSE.toString();
-
-  /** stricter function than {@link Boolean#parseBoolean(String)}
-   * 
-   * @param string
-   * @return null if string does not equal "true" or "false" */
-  static Boolean booleanOrNull(String string) {
-    if (string.equals(FALSE))
-      return false;
-    return string.equals(TRUE) ? true : null;
-  }
-
   /** @param string
    * @return imported properties, or null if resource could not be loaded */
   static Properties load(File file) {
