c5ae67d2d31be160564c7e759495b7aa4ef15803
Jan Hakenberg <jan.hakenberg@gmail.com>
2018-05-19 22:06:04 +0200

    trajectory config

--- src/test/java/ch/ethz/idsc/gokart/core/pure/TrajectoryConfigTest.java
+++ src/test/java/ch/ethz/idsc/gokart/core/pure/TrajectoryConfigTest.java
@@ -5,6 +5,7 @@ import ch.ethz.idsc.retina.dev.steer.SteerConfig;
 import ch.ethz.idsc.retina.util.math.Magnitude;
 import ch.ethz.idsc.tensor.Scalar;
 import ch.ethz.idsc.tensor.qty.Quantity;
+import ch.ethz.idsc.tensor.sca.Sign;
 import junit.framework.TestCase;
 
 public class TrajectoryConfigTest extends TestCase {
@@ -12,5 +13,17 @@ public class TrajectoryConfigTest extends TestCase {
     Scalar scalar = //
         Magnitude.PER_METER.apply(TrajectoryConfig.GLOBAL.maxRotation);
     SteerConfig.GLOBAL.getAngleLimit().requireInside(Quantity.of(scalar, "rad"));
+  }
+
+  public void testCutoff() {
+    Scalar scalar = TrajectoryConfig.GLOBAL.getCutoffDistance(Quantity.of(3, "m*s^-1"));
+    Magnitude.METER.apply(scalar);
+    Sign.requirePositive(scalar);
+  }
+
+  public void testCutoffNonNegative() {
+    Scalar scalar = TrajectoryConfig.GLOBAL.getCutoffDistance(Quantity.of(-5.3, "m*s^-1"));
+    Magnitude.METER.apply(scalar);
+    Sign.requirePositive(scalar);
   }
 }
