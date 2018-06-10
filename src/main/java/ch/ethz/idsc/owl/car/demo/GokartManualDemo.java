7d9cb90f16224671b14330fcfe6c82c50493e387
Jan Hakenberg <jan.hakenberg@gmail.com>
2018-06-10 20:50:35 +0200

    20180610 waypoints

---
+++ src/main/java/ch/ethz/idsc/owl/car/demo/GokartManualDemo.java
@@ -0,0 +1,69 @@
+// code by jph
+package ch.ethz.idsc.owl.car.demo;
+
+import ch.ethz.idsc.owl.bot.se2.glc.GokartEntity;
+import ch.ethz.idsc.owl.bot.se2.glc.GokartVecEntity;
+import ch.ethz.idsc.owl.bot.se2.glc.HelperHangarMap;
+import ch.ethz.idsc.owl.bot.util.DemoInterface;
+import ch.ethz.idsc.owl.bot.util.RegionRenders;
+import ch.ethz.idsc.owl.glc.adapter.RegionConstraints;
+import ch.ethz.idsc.owl.glc.std.PlannerConstraint;
+import ch.ethz.idsc.owl.gui.RenderInterface;
+import ch.ethz.idsc.owl.gui.ren.MouseShapeRender;
+import ch.ethz.idsc.owl.gui.win.MouseGoal;
+import ch.ethz.idsc.owl.gui.win.OwlyAnimationFrame;
+import ch.ethz.idsc.owl.math.planar.ConeRegion;
+import ch.ethz.idsc.owl.math.region.RegionWithDistance;
+import ch.ethz.idsc.owl.math.state.SimpleTrajectoryRegionQuery;
+import ch.ethz.idsc.owl.math.state.StateTime;
+import ch.ethz.idsc.tensor.RealScalar;
+import ch.ethz.idsc.tensor.Tensor;
+import ch.ethz.idsc.tensor.Tensors;
+
+/** demo to simulate dubendorf hangar */
+public class GokartManualDemo implements DemoInterface {
+  private static final Tensor ARROWHEAD = Tensors.matrixDouble( //
+      new double[][] { { .3, 0 }, { -.1, -.1 }, { -.1, +.1 } }).multiply(RealScalar.of(2));
+  private static final Tensor MODEL2PIXEL = Tensors.matrixDouble(new double[][] { { 7.5, 0, 0 }, { 0, -7.5, 640 }, { 0, 0, 1 } });
+
+  @Override
+  public OwlyAnimationFrame start() {
+    OwlyAnimationFrame owlyAnimationFrame = new OwlyAnimationFrame();
+    final StateTime initial = new StateTime(Tensors.vector(33.6, 41.5, 0.6), RealScalar.ZERO);
+    GokartEntity gokartEntity = new GokartVecEntity(initial) {
+      @Override
+      public RegionWithDistance<Tensor> getGoalRegionWithDistance(Tensor goal) {
+        return new ConeRegion(goal, RealScalar.of(Math.PI / 10));
+      }
+    };
+    // ---
+    HelperHangarMap hangarMap = new HelperHangarMap("/map/dubendorf/hangar/20180610obstacles.png", gokartEntity);
+    // ---
+    // Tensor waypoints = ResourceData.of("/demo/dubendorf/hangar/20180425waypoints.csv");
+    PlannerConstraint plannerConstraint = RegionConstraints.timeInvariant(hangarMap.region);
+    MouseGoal.simple(owlyAnimationFrame, gokartEntity, plannerConstraint);
+    // ---
+    owlyAnimationFrame.add(gokartEntity);
+    owlyAnimationFrame.addBackground(RegionRenders.create(hangarMap.imageRegion));
+    owlyAnimationFrame.geometricComponent.setModel2Pixel(MODEL2PIXEL);
+    // ---
+    {
+      // RenderInterface renderInterface = new Se2WaypointRender(waypoints, ARROWHEAD, new Color(64, 192, 64, 64));
+      // owlyAnimationFrame.addBackground(renderInterface);
+    }
+    // GlcPlannerCallback glcPlannerCallback = new SimpleGlcPlannerCallback(gokartEntity);
+    {
+      RenderInterface renderInterface = new MouseShapeRender( //
+          SimpleTrajectoryRegionQuery.timeInvariant(hangarMap.region), //
+          GokartEntity.SHAPE, () -> gokartEntity.getStateTimeNow().time());
+      owlyAnimationFrame.addBackground(renderInterface);
+    }
+    owlyAnimationFrame.configCoordinateOffset(50, 700);
+    owlyAnimationFrame.jFrame.setBounds(100, 50, 1200, 800);
+    return owlyAnimationFrame;
+  }
+
+  public static void main(String[] args) {
+    new GokartManualDemo().start().jFrame.setVisible(true);
+  }
+}
