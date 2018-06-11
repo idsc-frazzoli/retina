2fe5234b46d4094c453da3abe171e815c08f5b95
Jan Hakenberg <jan.hakenberg@gmail.com>
2018-06-11 09:51:14 +0200

    simplification

--- src/main/java/ch/ethz/idsc/demo/jph/sys/GokartLcmLogPlayer.java
+++ src/main/java/ch/ethz/idsc/demo/jph/sys/GokartLcmLogPlayer.java
@@ -17,6 +17,7 @@ enum GokartLcmLogPlayer {
     file = UserHome.file("gokart/manual/20180427T105421_circle/log.lcm");
     file = DatahakiLogFileLocator.file(GokartLogFile._20180430T104113_a5291af9);
     file = DatahakiLogFileLocator.file(GokartLogFile._20180604T150508_15e65bba);
+    file = UserHome.file("gokart/pedestrian/20180604T150508/log.lcm");
     cfg.logFile = file.toString();
     cfg.speed_numerator = 1;
     cfg.speed_denominator = 2;
