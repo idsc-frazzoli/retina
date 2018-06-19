8e6a1f14b200b70f1b7670e9fe34a129493b179a
ValentinaCavinato <vcavinat@student.ethz.ch>
2018-06-19 18:46:26 +0200

    Step of arbitrary length in the future

--- src/main/java/ch/ethz/idsc/gokart/core/perc/LinearPredictor.java
+++ src/main/java/ch/ethz/idsc/gokart/core/perc/LinearPredictor.java
@@ -11,59 +11,81 @@ import ch.ethz.idsc.tensor.mat.Inverse;
 import ch.ethz.idsc.tensor.red.Mean;
 
 public class LinearPredictor {
-  private Tensor nextMean = Tensors.empty();
-  private Tensor nextHull = Tensors.empty();
-  private Tensor nextMean1 = Tensors.empty();// TODO: prediction 2 time steps in the future
-  private Tensor nextHull1 = Tensors.empty(); // TODO: same as above
+  private Tensor nextMeans = Tensors.empty();
+  private Tensor nextHulls = Tensors.empty();
+  private double step;
+  private Tensor twiceNextMeans = Tensors.empty();// prediction 2 time steps in the future
+  private Tensor twiceNextHulls = Tensors.empty();
 
   public LinearPredictor(ClusterCollection collection, double step) {
+    this.step = step;
     for (ClusterDeque clusterDeque : collection.getCollection()) {
       Tensor nonEmptyMeans = clusterDeque.getNonEmptyMeans();
       Tensor hull = clusterDeque.getLast().hull();
-      Tensor predictNextMean = predictNextMean(nonEmptyMeans, step);
-      if (Tensors.nonEmpty(predictNextMean))
-        nextMean.append(predictNextMean);
-      if (Tensors.nonEmpty(hull) && Tensors.nonEmpty(nextMean))
-        nextHull.append(predictNextHull(hull));
+      predictNextMean(nonEmptyMeans);
+      if (Tensors.nonEmpty(nextMeans) && Tensors.nonEmpty(twiceNextMeans))
+        predictNextHull(hull);
     }
   }
 
-  // linear prediction: linear regression in closed form, from the last mean take a step of length st.
-  private Tensor predictNextMean(Tensor nonEmptyMeans, double st) {
+  private void predictNextMean(Tensor nonEmptyMeans) {
+    Tensor predictMean = predictMean(nonEmptyMeans, 1);
+    if (Tensors.nonEmpty(predictMean)) {
+      nextMeans.append(predictMean);
+      twiceNextMeans.append(predictMean(nonEmptyMeans, 2));
+    }
+  }
+
+  // linear prediction: linear regression in closed form, from the last mean take a step of length i*step
+  private Tensor predictMean(Tensor nonEmptyMeans, int i) {
     Tensor x = Tensor.of(nonEmptyMeans.stream().map(tensor -> Tensors.of(tensor.Get(0), RealScalar.of(1))));// homogeneous coordinates
     Tensor y = Tensor.of(nonEmptyMeans.stream().map(tensor -> tensor.Get(1)));
     if (Tensors.nonEmpty(x)) {
       if (x.length() > 1) { // fit a line if more than one point
         Tensor beta = Inverse.of(Transpose.of(x).dot(x)).dot(Transpose.of(x).dot(y));
         double b = beta.Get(0).number().doubleValue();
-        double nextX = Last.of(nonEmptyMeans).Get(0).number().doubleValue() + st * Math.cos(b);
-        double nextY = Last.of(nonEmptyMeans).Get(1).number().doubleValue() + st * Math.sin(b);
+        double nextX = Last.of(nonEmptyMeans).Get(0).number().doubleValue() + i * step * Math.cos(b);
+        double nextY = Last.of(nonEmptyMeans).Get(1).number().doubleValue() + i * step * Math.sin(b);
         return Tensors.vectorDouble(nextX, nextY);
       } else
-        return Flatten.of(nonEmptyMeans); // if only one point assume it is not going to move
+        return Flatten.of(nonEmptyMeans);// if only one point assume it is not going to move
     }
     return Tensors.empty();
   }
 
   // assign last hull shape to the new mean
-  private Tensor predictNextHull(Tensor hull) {
+  private void predictNextHull(Tensor hull) {
     if (Tensors.nonEmpty(hull)) {
       Tensor mean = Mean.of(hull);
       Tensor next = Tensors.empty();
-      for (Tensor x : hull) {
-        Tensor y = x.subtract(mean);
-        next.append(Last.of(nextMean).add(y));
+      Tensor twiceNext = Tensors.empty();
+      Tensor nm = Last.of(nextMeans);
+      Tensor tnm = Last.of(twiceNextMeans);
+      if (Tensors.nonEmpty(Last.of(nm))) {
+        for (Tensor x : hull) {
+          Tensor y = x.subtract(mean);
+          next.append(nm.add(y));
+          twiceNext.append(tnm.add(y));
+        }
+        nextHulls.append(next);
+        twiceNextHulls.append(twiceNext);
       }
-      return next;
     }
-    return Tensors.empty();
   }
 
   public Tensor getMeanPredictions() {
-    return nextMean.unmodifiable();
+    return nextMeans.unmodifiable();
   }
 
   public Tensor getHullPredictions() {
-    return nextHull.unmodifiable();
+    return nextHulls.unmodifiable();
+  }
+
+  public Tensor getTNMeanPredictions() {
+    return twiceNextMeans.unmodifiable();
+  }
+
+  public Tensor getTNHullPredictions() {
+    return twiceNextHulls.unmodifiable();
   }
 }
