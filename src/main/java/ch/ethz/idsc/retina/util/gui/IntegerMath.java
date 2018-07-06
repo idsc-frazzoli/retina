55faf685fffd5506fa3fbf34d4b078e82b2d3e15
Jan Hakenberg <jan.hakenberg@gmail.com>
2018-07-06 12:53:49 +0200

    remove unused functions

--- src/main/java/ch/ethz/idsc/retina/util/gui/IntegerMath.java
+++ src/main/java/ch/ethz/idsc/retina/util/gui/IntegerMath.java
@@ -1,8 +1,6 @@
 // code by jph
 package ch.ethz.idsc.retina.util.gui;
 
-import java.util.Collection;
-
 enum IntegerMath {
   ;
   /** mod that behaves like in Matlab. for instance mod(-10, 3) == 2
@@ -15,30 +13,5 @@ enum IntegerMath {
     // if value is below 0, then -size < value && value < 0.
     // For instance: -3%3==0, and -2%3==-2.
     return value < 0 ? size + value : value;
-  }
-
-  /** Euclid's algorithm
-   * 
-   * @param a
-   * @param b
-   * @return greatest common divider of a and b. */
-  public static int gcd(int a, int b) {
-    return b == 0 ? a : gcd(b, a % b);
-  }
-
-  public static int lcm(int a, int b) {
-    return a * (b / gcd(a, b)); // to avoid overflow
-  }
-
-  /** @param myCollection non-empty
-   * @return greatest common divider of all integers in myCollection */
-  public static int gcd(Collection<Integer> myCollection) {
-    return myCollection.stream().reduce(IntegerMath::gcd).orElse(null);
-  }
-
-  /** @param myCollection non-empty
-   * @return least common multiple of all integers in myCollection */
-  public static int lcm(Collection<Integer> myCollection) {
-    return myCollection.stream().reduce(IntegerMath::lcm).orElse(null);
   }
 }
