d9e5f4bfe240169ebabfa34d4afdbe8ac321c428
Marc Heim <heimm@ethz.ch>
2018-09-28 22:09:59 +0200

    Added comment and fixed error.

--- src/main/java/ch/ethz/idsc/retina/dev/steer/SteerConfig.java
+++ src/main/java/ch/ethz/idsc/retina/dev/steer/SteerConfig.java
@@ -47,8 +47,9 @@ public class SteerConfig implements Serializable {
   public Scalar columnMax = Quantity.of(0.7, SteerPutEvent.UNIT_ENCODER);
   /** conversion factor from measured steer column angle to front wheel angle */
   public Scalar column2steer = Quantity.of(0.6, "rad*SCE^-1");
-  /** factors for advanced Steering function */
+  /** linear factor for advanced Steering function */
   public Scalar advColumn2steer1 = Quantity.of(0.93, "rad*SCE^-1");
+  /** cubic factor for advanced Steering function */
   public Scalar advColumn2steer3 = Quantity.of(-0.58, "rad*SCE^-3");
   /** 0.5 corresponds to 50% of torque limit */
   public Scalar stepOfLimit = RealScalar.of(0.5);
@@ -77,7 +78,7 @@ public class SteerConfig implements Serializable {
     Scalar linearComponent = Times.of(CSEangle, advColumn2steer1);
     Scalar cubicComponent = Times.of(CSEangle,CSEangle, CSEangle, advColumn2steer3);
     return UnitSystem.SI().apply(
-        linearComponent.add(cubicComponent);
+        linearComponent.add(cubicComponent));
   }
 
   /** @return scalar without unit but with interpretation in radians
