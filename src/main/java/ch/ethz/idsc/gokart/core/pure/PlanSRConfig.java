338640afce9e76ed5968c99b4397f83a07d502ca
Jan Hakenberg <jan.hakenberg@gmail.com>
2018-09-29 14:09:49 +0200

    sync config classes

--- src/main/java/ch/ethz/idsc/gokart/core/pure/PlanSRConfig.java
+++ src/main/java/ch/ethz/idsc/gokart/core/pure/PlanSRConfig.java
@@ -7,11 +7,12 @@ import ch.ethz.idsc.tensor.Scalar;
 public class PlanSRConfig {
   public static final PlanSRConfig GLOBAL = AppResources.load(new PlanSRConfig());
   // ---
+  // TODO units
   public Boolean SR_PED_LEGAL = true;
   public Boolean SR_PED_ILLEGAL = false;
   public Scalar PED_VELOCITY = RealScalar.of(1.6);
   public Scalar CAR_VELOCITY = RealScalar.of(10);
   public Scalar PED_RADIUS = RealScalar.of(0.3);
   public Scalar MAX_A = RealScalar.of(5.0); // [m/s²]
-  public Scalar REACTION_TIME = RealScalar.of(0.3);
+  public Scalar REACTION_TIME = RealScalar.of(0.6);
 }
