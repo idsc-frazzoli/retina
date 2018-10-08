ef7426a3d1b0d5456649a3ae0b45726704523d05
Marc Heim <heimm@ethz.ch>
2018-10-08 11:34:28 +0200

    Forces should now be compiled in conjunction with the server. It does not yet produce sensible results.

--- src_MATLAB/MPCGokart/ForcesMPCPathFollowing/CompileNativeMPC.sh
+++ src_MATLAB/MPCGokart/ForcesMPCPathFollowing/CompileNativeMPC.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-gcc nativeexecution.c -o nativeMPC
-gcc nativeexecutionclienttest.c -o clienttest
+echo 'compiling native execution'
+gcc nativeexecution.c -o nativeMPC MPCPathFollowing_casadi2forces.o MPCPathFollowing_model_1.o MPCPathFollowing_model_31.o MPCPathFollowing/obj/MPCPathFollowing.o -lm
+#gcc nativeexecutionclienttest.c -o clienttest
