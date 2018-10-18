f6d5284babeb27f670e5c10f995b6812ef445722
Jan Hakenberg <jan.hakenberg@gmail.com>
2018-10-18 15:46:30 +0200

    edit mpc path following test

--- src/test/java/ch/ethz/idsc/gokart/core/mpc/MPCPathFollowingClientLCMTest.java
+++ src/test/java/ch/ethz/idsc/gokart/core/mpc/MPCPathFollowingClientLCMTest.java
@@ -8,24 +8,29 @@ public class MPCPathFollowingClientLCMTest extends TestCase {
     // only sends a simple message
     // uncomment if you are able to compile the binary
     LcmMPCPathFollowingClient lcmMPCPathFollowingClient = new LcmMPCPathFollowingClient();
-    lcmMPCPathFollowingClient.start();
-    for (int i = 0; i < 4; i++) {
-      System.out.println("i=" + i);
-      GokartState gokartState = new GokartState(//
-          11, //
-          12, //
-          13, //
-          14, //
-          15, //
-          16, //
-          17, //
-          18, //
-          19, //
-          20);
-      lcmMPCPathFollowingClient.publishGokartState(gokartState);
-      Thread.sleep(1000);
-      // System.out.print(lcmMPCPathFollowingClient.mpcNativeSession.getNativeOutput());
+    try {
+      // TODO jan had to add try here
+      lcmMPCPathFollowingClient.start();
+      for (int i = 0; i < 4; i++) {
+        System.out.println("i=" + i);
+        GokartState gokartState = new GokartState(//
+            11, //
+            12, //
+            13, //
+            14, //
+            15, //
+            16, //
+            17, //
+            18, //
+            19, //
+            20);
+        lcmMPCPathFollowingClient.publishGokartState(gokartState);
+        Thread.sleep(1000);
+        // System.out.print(lcmMPCPathFollowingClient.mpcNativeSession.getNativeOutput());
+      }
+      lcmMPCPathFollowingClient.stop();
+    } catch (Exception exception) {
+      exception.printStackTrace();
     }
-    lcmMPCPathFollowingClient.stop();
   }
 }
