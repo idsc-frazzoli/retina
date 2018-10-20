af877e595ca9222556f2d732cb8e22ed2fb86c5b
Marc Heim <marcheim7@gmail.com>
2018-10-20 13:10:42 +0200

    Implemented LookupTableRimoThrust

--- src/main/java/ch/ethz/idsc/gokart/core/joy/LookupTableRimoThrustJoystickModule.java
+++ src/main/java/ch/ethz/idsc/gokart/core/joy/LookupTableRimoThrustJoystickModule.java
@@ -4,25 +4,34 @@ package ch.ethz.idsc.gokart.core.joy;
 import java.util.Optional;
 
 import ch.ethz.idsc.gokart.core.mpc.PowerLookupTable;
+import ch.ethz.idsc.gokart.gui.top.ChassisGeometry;
 import ch.ethz.idsc.retina.dev.joystick.GokartJoystickInterface;
+import ch.ethz.idsc.retina.dev.rimo.RimoGetEvent;
+import ch.ethz.idsc.retina.dev.rimo.RimoGetListener;
 import ch.ethz.idsc.retina.dev.rimo.RimoPutEvent;
 import ch.ethz.idsc.retina.dev.rimo.RimoPutHelper;
 import ch.ethz.idsc.retina.dev.rimo.RimoSocket;
 import ch.ethz.idsc.retina.dev.steer.SteerColumnInterface;
 import ch.ethz.idsc.retina.util.math.Magnitude;
+import ch.ethz.idsc.retina.util.math.SI;
 import ch.ethz.idsc.tensor.RealScalar;
 import ch.ethz.idsc.tensor.Scalar;
 import ch.ethz.idsc.tensor.alg.Differences;
+import ch.ethz.idsc.tensor.qty.Quantity;
 
-public class LookupTableRimoThrustJoystickModule extends GuideJoystickModule<RimoPutEvent> {
+public class LookupTableRimoThrustJoystickModule extends GuideJoystickModule<RimoPutEvent> implements RimoGetListener {
+  private Scalar meanTangentSpeed = Quantity.of(0, SI.VELOCITY);
+
   @Override // from AbstractModule
   void protected_first() {
     RimoSocket.INSTANCE.addPutProvider(this);
+    RimoSocket.INSTANCE.addGetListener(this);
   }
 
   @Override // from AbstractModule
   void protected_last() {
     RimoSocket.INSTANCE.removePutProvider(this);
+    RimoSocket.INSTANCE.removeGetListener(this);
   }
 
   PowerLookupTable powerLookupTable = PowerLookupTable.getInstance();
@@ -35,12 +44,18 @@ public class LookupTableRimoThrustJoystickModule extends GuideJoystickModule<Rim
     // Scalar pair = joystick.getAheadPair_Unit().Get(1); // entry in [0, 1]
     pair = pair.multiply(JoystickConfig.GLOBAL.torqueLimit);
     // get the wanted acceleration
-    Scalar wantedAcceleration = powerLookupTable.getNormalizedAcceleration(pair, RealScalar.ZERO);
+    Scalar wantedAcceleration = powerLookupTable.getNormalizedAcceleration(pair, meanTangentSpeed);
+    Scalar current = powerLookupTable.getNeededCurrent(wantedAcceleration, meanTangentSpeed);
     // get the
     short arms_raw = Magnitude.ARMS.toShort(pair); // confirm that units are correct
     return Optional.of(RimoPutHelper.operationTorque( //
         (short) -arms_raw, // sign left invert
         (short) +arms_raw // sign right id
     ));
+  }
+
+  @Override // from RimoGetListener
+  public final void getEvent(RimoGetEvent getEvent) {
+    meanTangentSpeed = ChassisGeometry.GLOBAL.odometryTangentSpeed(getEvent);
   }
 }
