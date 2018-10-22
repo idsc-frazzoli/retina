0ea39a97f6d8ec87961cceebcaa165b770484787
Marc Heim <marcheim7@gmail.com>
2018-10-22 14:16:25 +0200

    Implemented Lookup Rimo Rate Controller

--- src/main/java/ch/ethz/idsc/retina/dev/rimo/LookupRimoRateController.java
+++ src/main/java/ch/ethz/idsc/retina/dev/rimo/LookupRimoRateController.java
@@ -4,6 +4,7 @@ package ch.ethz.idsc.retina.dev.rimo;
 
 import ch.ethz.idsc.gokart.core.mpc.PowerLookupTable;
 import ch.ethz.idsc.gokart.gui.GokartLcmChannel;
+import ch.ethz.idsc.gokart.gui.top.ChassisGeometry;
 import ch.ethz.idsc.retina.lcm.BinaryBlobPublisher;
 import ch.ethz.idsc.retina.lcm.VectorFloatBlob;
 import ch.ethz.idsc.retina.util.math.NonSI;
@@ -36,27 +37,29 @@ import ch.ethz.idsc.tensor.sca.Clip;
     this.rimoConfig = rimoConfig;
   }
 
-  Scalar integral = Quantity.of(0, SI.METER);
+  //Public because of testing
+  public Scalar integral = Quantity.of(0, SI.METER);
   Scalar velocity = Quantity.of(0, SI.VELOCITY);
 
   @Override
   public void setVelocity(Scalar abs_vel) {
     // TODO Auto-generated method stub
-    RimoRateController.super.setVelocity(abs_vel);
+    velocity = abs_vel.multiply(ChassisGeometry.GLOBAL.tireRadiusRear);
   }
 
   @Override // from RimoRateController
   public Scalar iterate(final Scalar vel_error) {
-    final Scalar pPart = vel_error.subtract(lastVel_error).multiply(rimoConfig.Kp);
-    final Scalar iPart = integral.multiply(rimoConfig.Ki);
-    final Scalar acc_value = lastTor_value.add(pPart).add(iPart);
+    final Scalar tangentVelError = ChassisGeometry.GLOBAL.tireRadiusRear.multiply(vel_error);
+    final Scalar pPart = tangentVelError.multiply(rimoConfig.lKp);
+    final Scalar iPart = integral.multiply(rimoConfig.lKi);
+    final Scalar acc_value = pPart.add(iPart);
     final Scalar currentValue = lookupTable.getNeededCurrent(acc_value, velocity);
     // get min and max aviable
     Tensor minmax = lookupTable.getMinMaxAcceleration(velocity);
     // anti windup
     if (Scalars.lessThan(minmax.Get(0), acc_value) && Scalars.lessThan(acc_value, minmax.Get(1))) {
       // update integral
-      integral = integral.add(vel_error.multiply(DT));
+      integral = integral.add(tangentVelError.multiply(DT));
     }
     binaryBlobPublisher.accept(VectorFloatBlob.encode(Tensors.of( //
         vel_error, pPart, iPart, integral, lastTor_value)));
