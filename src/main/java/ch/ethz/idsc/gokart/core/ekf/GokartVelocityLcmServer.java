f220fa16174d918dcb772db33b03108300e764c5
Marc Heim <marcheim7@gmail.com>
2019-01-25 11:10:55 +0100

    Velocity server (WIP)

--- src/main/java/ch/ethz/idsc/gokart/core/ekf/GokartVelocityLcmServer.java
+++ src/main/java/ch/ethz/idsc/gokart/core/ekf/GokartVelocityLcmServer.java
@@ -1,7 +1,7 @@
 // code by jph
 package ch.ethz.idsc.gokart.core.ekf;
 
-import ch.ethz.idsc.gokart.core.pos.GokartPoseEvent;
+import ch.ethz.idsc.gokart.core.pos.GokartPoseLcmModule;
 import ch.ethz.idsc.gokart.gui.GokartLcmChannel;
 import ch.ethz.idsc.gokart.lcm.BinaryBlobPublisher;
 
@@ -14,8 +14,8 @@ public enum GokartVelocityLcmServer {
 
   public void publish() {
     // TODO: maybe move that to other place
-    GokartVelocityEvent gokartVelocityEvent = GokartVelocityEvents.getPoseEvent(velocity)
-    publish(odometryRimoGetLcmClient.gokartPoseOdometry.getPoseEvent());
+    //GokartVelocityEvent gokartVelocityEvent = GokartVelocityEvents.getPoseEvent(velocity)
+    //publish(odometryRimoGetLcmClient.gokartPoseOdometry.getPoseEvent());
   }
 
   public void publish(GokartVelocityEvent gokartVelocityEvent) {
