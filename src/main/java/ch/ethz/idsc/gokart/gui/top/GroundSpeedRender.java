67e23d85594c240a64dcad7658b932b269ceac36
Jan Hakenberg <jan.hakenberg@gmail.com>
2019-01-28 08:07:15 +0100

    acceleration render precompute color gradient

--- src/main/java/ch/ethz/idsc/gokart/gui/top/GroundSpeedRender.java
+++ src/main/java/ch/ethz/idsc/gokart/gui/top/GroundSpeedRender.java
@@ -9,20 +9,17 @@ import ch.ethz.idsc.gokart.core.ekf.VelocityEstimation;
 import ch.ethz.idsc.owl.gui.RenderInterface;
 import ch.ethz.idsc.owl.gui.win.GeometricLayer;
 import ch.ethz.idsc.retina.util.sys.ModuleAuto;
-import ch.ethz.idsc.sophus.filter.GeodesicIIR1Filter;
-import ch.ethz.idsc.sophus.group.RnGeodesic;
 import ch.ethz.idsc.sophus.group.Se2Utils;
 import ch.ethz.idsc.tensor.RealScalar;
 import ch.ethz.idsc.tensor.Scalar;
 import ch.ethz.idsc.tensor.Tensor;
 import ch.ethz.idsc.tensor.Tensors;
+import ch.ethz.idsc.tensor.alg.Array;
 
-public class GroundSpeedRender implements RenderInterface {
-  private final Tensor origin = Tensors.of(RealScalar.ZERO, RealScalar.ZERO);
-  private final Scalar scale = RealScalar.of(0.1);
+/* package */ class GroundSpeedRender implements RenderInterface {
+  private static final Tensor ORIGIN = Array.zeros(2);
+  private static final Scalar SCALE = RealScalar.of(0.1);
   // ---
-  private final GeodesicIIR1Filter geodesicIIR1Filter = //
-      new GeodesicIIR1Filter(RnGeodesic.INSTANCE, RealScalar.of(.02));
   private final Tensor xya;
 
   public GroundSpeedRender(Tensor xya) {
@@ -35,8 +32,9 @@ public class GroundSpeedRender implements RenderInterface {
     geometricLayer.pushMatrix(Se2Utils.toSE2Matrix(xya));
     graphics.setColor(Color.BLUE);
     Tensor velocity = velocityEstimation.getVelocity();
+    // TODO MH why rebuild the tensor and not extract?
     Tensor velocityXY = Tensors.of(velocity.Get(0), velocity.Get(1));
-    Tensor line = Tensors.of(origin, velocityXY.multiply(scale));
+    Tensor line = Tensors.of(ORIGIN, velocityXY.multiply(SCALE));
     graphics.draw(geometricLayer.toPath2D(line));
     geometricLayer.popMatrix();
   }
