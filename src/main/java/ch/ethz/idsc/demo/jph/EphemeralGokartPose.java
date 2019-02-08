13f4fae1145324d68a69fc1f1dabd509ae025772
Jan Hakenberg <jan.hakenberg@gmail.com>
2019-02-08 11:28:35 +0100

    ephemeral pose index

--- src/main/java/ch/ethz/idsc/demo/jph/EphemeralGokartPose.java
+++ src/main/java/ch/ethz/idsc/demo/jph/EphemeralGokartPose.java
@@ -5,30 +5,28 @@ import java.io.File;
 import java.io.IOException;
 
 import ch.ethz.idsc.gokart.lcm.OfflineLogPlayer;
-import ch.ethz.idsc.gokart.offline.api.GokartLogAdapter;
-import ch.ethz.idsc.gokart.offline.api.GokartLogInterface;
 import ch.ethz.idsc.gokart.offline.api.OfflineTableSupplier;
-import ch.ethz.idsc.gokart.offline.channel.GokartPoseChannel;
-import ch.ethz.idsc.gokart.offline.tab.SingleChannelTable;
 import ch.ethz.idsc.tensor.Tensor;
 import ch.ethz.idsc.tensor.io.CsvFormat;
 import ch.ethz.idsc.tensor.io.Export;
 import ch.ethz.idsc.tensor.io.HomeDirectory;
+import ch.ethz.idsc.tensor.sca.Round;
 
 /* package */ enum EphemeralGokartPose {
   ;
   public static void main(String[] args) throws IOException {
-    File root = new File("/media/datahaki/media/ethz/gokart/topic/localization");
-    File dest = HomeDirectory.file("Projects/ephemeral/src/main/resources/dubilab/app/pose/gyro");
+    File root = new File("/media/datahaki/data/gokart/cuts/20190204");
     for (File folder : root.listFiles())
       if (folder.isDirectory()) {
-        System.out.println(folder);
-        GokartLogInterface gokartLogInterface = GokartLogAdapter.of(folder);
-        // ---
-        OfflineTableSupplier offlineTableSupplier = SingleChannelTable.of(GokartPoseChannel.INSTANCE);
-        OfflineLogPlayer.process(gokartLogInterface.file(), offlineTableSupplier);
-        Tensor tensor = offlineTableSupplier.getTable().map(CsvFormat.strict());
-        Export.of(new File(dest, folder.getName() + ".csv"), tensor);
+        File file = new File(folder, "post.lcm");
+        if (file.isFile()) {
+          OfflineTableSupplier offlineTableSupplier = new UniqueTimePoseQualityTable();
+          OfflineLogPlayer.process(file, offlineTableSupplier);
+          Tensor tensor = offlineTableSupplier.getTable().map(CsvFormat.strict());
+          File dest = HomeDirectory.file("Projects/ephemeral/src/main/resources/dubilab/app/pose/5m");
+          Export.of(new File(dest, folder.getName() + ".csv"), tensor.map(Round._6));
+        } else
+          System.err.println("skip " + folder);
       }
   }
 }
