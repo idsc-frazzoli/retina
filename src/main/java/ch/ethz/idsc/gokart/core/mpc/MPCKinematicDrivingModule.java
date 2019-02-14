663b4e7121fa5e67f09a57544ba55b00583b80f0
datahaki <jan.hakenberg@gmail.com>
2019-02-14 15:21:35 +0100

    Small changes for demp

--- src/main/java/ch/ethz/idsc/gokart/core/mpc/MPCKinematicDrivingModule.java
+++ src/main/java/ch/ethz/idsc/gokart/core/mpc/MPCKinematicDrivingModule.java
@@ -25,8 +25,8 @@ public class MPCKinematicDrivingModule extends AbstractModule implements MPCBSpl
       ModuleAuto.INSTANCE.getInstance(TrackReconModule.class);
   public final LcmMPCControlClient lcmMPCPathFollowingClient = new LcmMPCControlClient();
   private final MPCOptimizationConfig mpcPathFollowingConfig = MPCOptimizationConfig.GLOBAL;
-  private final MPCSteering mpcSteering = new MPCOpenLoopSteering();
-  // private final MPCSteering mpcSteering = new MPCCorrectedOpenLoopSteering();
+  //private final MPCSteering mpcSteering = new MPCOpenLoopSteering();
+  private final MPCSteering mpcSteering = new MPCCorrectedOpenLoopSteering();
   // private final MPCBraking mpcBraking = new MPCSimpleBraking();
   // private final MPCBraking mpcBraking = new MPCAggressiveTorqueVectoringBraking();
   private final MPCBraking mpcBraking = new MPCAggressiveCorrectedTorqueVectoringBraking();
