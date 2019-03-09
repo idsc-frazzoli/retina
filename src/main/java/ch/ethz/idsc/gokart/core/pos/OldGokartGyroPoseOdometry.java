6706dbf22858c0d11e9f933562724e9b4594b3c2
Jan Hakenberg <jan.hakenberg@gmail.com>
2019-03-09 10:59:04 +0100

    velocity estimation in pose integrator

--- src/main/java/ch/ethz/idsc/gokart/core/pos/OldGokartGyroPoseOdometry.java
+++ src/main/java/ch/ethz/idsc/gokart/core/pos/OldGokartGyroPoseOdometry.java
@@ -1,14 +1,10 @@
 // code by jph
 package ch.ethz.idsc.gokart.core.pos;
 
-import java.util.Objects;
-
-import ch.ethz.idsc.gokart.core.ekf.SimplePositionVelocityModule;
 import ch.ethz.idsc.gokart.core.fuse.DavisImuTracker;
 import ch.ethz.idsc.owl.bot.se2.Se2StateSpaceModel;
 import ch.ethz.idsc.owl.math.StateSpaceModels;
 import ch.ethz.idsc.owl.math.flow.Flow;
-import ch.ethz.idsc.retina.util.sys.ModuleAuto;
 import ch.ethz.idsc.tensor.RealScalar;
 import ch.ethz.idsc.tensor.Scalar;
 import ch.ethz.idsc.tensor.Tensor;
@@ -19,9 +15,9 @@ import ch.ethz.idsc.tensor.sca.N;
  * and uses low-pass signal of gyro measurement for vehicle turn rate.
  * 
  * This combination was found to be superior over pure wheel odometry. */
-/* package */ class GokartGyroPoseOdometry extends GokartPoseOdometry {
+/* package */ class OldGokartGyroPoseOdometry extends GokartPoseOdometry {
   public static GokartPoseOdometry create(Tensor state) {
-    return new GokartGyroPoseOdometry(state);
+    return new OldGokartGyroPoseOdometry(state);
   }
 
   public static GokartPoseOdometry create() {
@@ -29,12 +25,8 @@ import ch.ethz.idsc.tensor.sca.N;
   }
 
   // ---
-  private final SimplePositionVelocityModule simplePositionVelocityModule = //
-      ModuleAuto.INSTANCE.getInstance(SimplePositionVelocityModule.class);
-
-  GokartGyroPoseOdometry(Tensor state) {
+  OldGokartGyroPoseOdometry(Tensor state) {
     super(state);
-    System.out.println("uses vel est " + Objects.nonNull(simplePositionVelocityModule));
   }
 
   /** .
@@ -44,11 +36,9 @@ import ch.ethz.idsc.tensor.sca.N;
    * @return */
   @Override
   Flow singleton(Scalar speedL, Scalar speedR, Scalar yHalfWidth) {
-    final Scalar rate = DavisImuTracker.INSTANCE.getGyroZ();
-    // TODO also use rate from other Vmu931
-    Tensor x = Objects.isNull(simplePositionVelocityModule) //
-        ? Tensors.of(speedL.add(speedR).multiply(HALF), RealScalar.ZERO, rate)
-        : simplePositionVelocityModule.getXYVelocity().append(rate);
-    return StateSpaceModels.createFlow(Se2StateSpaceModel.INSTANCE, N.DOUBLE.of(x));
+    Scalar speed = speedL.add(speedR).multiply(HALF);
+    Scalar rate = DavisImuTracker.INSTANCE.getGyroZ();
+    return StateSpaceModels.createFlow(Se2StateSpaceModel.INSTANCE, //
+        N.DOUBLE.of(Tensors.of(speed, RealScalar.ZERO, rate)));
   }
 }
