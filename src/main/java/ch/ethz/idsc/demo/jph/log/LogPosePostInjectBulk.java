30ede6114041bf00c389c68ba937c1e952b20f2a
Jan Hakenberg <jan.hakenberg@gmail.com>
2019-03-09 10:26:27 +0100

    log file index

--- src/main/java/ch/ethz/idsc/demo/jph/log/LogPosePostInjectBulk.java
+++ src/main/java/ch/ethz/idsc/demo/jph/log/LogPosePostInjectBulk.java
@@ -6,7 +6,7 @@ import java.io.File;
 /* package */ enum LogPosePostInjectBulk {
   ;
   public static void main(String[] args) throws Exception {
-    final File root = new File(StaticHelper.CUTS, "20190307");
+    final File root = new File(StaticHelper.CUTS, "20190308");
     for (File folder : root.listFiles())
       if (folder.isDirectory()) {
         System.out.println(folder);
