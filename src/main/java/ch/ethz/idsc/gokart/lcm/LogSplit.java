d4a82469826d3e54895da0630e81cb960d45c974
Jan Hakenberg <jan.hakenberg@gmail.com>
2019-03-18 21:39:01 +0100

    refactor log split, log split predicate

---
+++ src/main/java/ch/ethz/idsc/gokart/lcm/LogSplit.java
@@ -0,0 +1,36 @@
+// code by jph
+package ch.ethz.idsc.gokart.lcm;
+
+import java.nio.ByteBuffer;
+import java.util.Collections;
+import java.util.NavigableMap;
+import java.util.Objects;
+import java.util.TreeMap;
+
+import ch.ethz.idsc.tensor.Scalar;
+
+public class LogSplit implements OfflineLogListener {
+  private final NavigableMap<Integer, Integer> navigableMap = new TreeMap<>();
+  private final LogSplitPredicate logSplitPredicate;
+  private int index = 0;
+  private Integer lo = null;
+
+  public LogSplit(LogSplitPredicate logSplitPredicate) {
+    this.logSplitPredicate = logSplitPredicate;
+  }
+
+  @Override // from OfflineLogListener
+  public void event(Scalar time, String channel, ByteBuffer byteBuffer) {
+    boolean crossing = logSplitPredicate.split(time, channel, byteBuffer);
+    if (crossing) {
+      if (Objects.nonNull(lo))
+        navigableMap.put(lo, index - 1);
+      lo = index;
+    }
+    ++index;
+  }
+
+  public NavigableMap<Integer, Integer> navigableMap() {
+    return Collections.unmodifiableNavigableMap(navigableMap);
+  }
+}
---
+++ src/test/java/ch/ethz/idsc/gokart/core/map/LapLogSplitPredicateTest.java
@@ -0,0 +1,25 @@
+// code by jph
+package ch.ethz.idsc.gokart.core.map;
+
+import ch.ethz.idsc.gokart.offline.pose.LapLogSplitPredicate;
+import ch.ethz.idsc.retina.util.math.SI;
+import ch.ethz.idsc.tensor.RealScalar;
+import ch.ethz.idsc.tensor.Tensor;
+import ch.ethz.idsc.tensor.Tensors;
+import ch.ethz.idsc.tensor.lie.AngleVector;
+import ch.ethz.idsc.tensor.qty.Quantity;
+import junit.framework.TestCase;
+
+public class LapLogSplitPredicateTest extends TestCase {
+  public void testSimple() {
+    LapLogSplitPredicate lapLogSplitPredicate = new LapLogSplitPredicate( //
+        Tensors.vector(41.6, 34.2).multiply(Quantity.of(1, SI.METER)), //
+        AngleVector.of(RealScalar.of(-2.25)));
+    Tensor newPos = Tensors.vector(20, 20).multiply(Quantity.of(1, SI.METER));
+    Tensor oldPos = Tensors.vector(50, 40).multiply(Quantity.of(1, SI.METER));
+    assertFalse(lapLogSplitPredicate.getLineTrigger(newPos, oldPos));
+    assertFalse(lapLogSplitPredicate.getLineTrigger(newPos, newPos));
+    assertFalse(lapLogSplitPredicate.getLineTrigger(oldPos, oldPos));
+    assertTrue(lapLogSplitPredicate.getLineTrigger(oldPos, newPos));
+  }
+}
