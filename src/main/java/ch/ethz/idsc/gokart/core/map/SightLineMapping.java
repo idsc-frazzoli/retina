1d5b4b881a388b39cfbc9b2370cbe185691fcfe0
JoelGaechter <joelgaechter@bluewin.ch>
2019-03-19 17:17:18 +0100

    fist iteration sight line mapping

---
+++ src/main/java/ch/ethz/idsc/gokart/core/map/SightLineMapping.java
@@ -0,0 +1,165 @@
+// code by gjoel
+package ch.ethz.idsc.gokart.core.map;
+
+import ch.ethz.idsc.gokart.core.perc.PolarObstaclePredicate;
+import ch.ethz.idsc.gokart.core.pos.GokartPoseEvent;
+import ch.ethz.idsc.gokart.core.pos.GokartPoseHelper;
+import ch.ethz.idsc.gokart.core.pos.GokartPoseLcmClient;
+import ch.ethz.idsc.gokart.core.pos.GokartPoseListener;
+import ch.ethz.idsc.gokart.gui.top.SensorsConfig;
+import ch.ethz.idsc.gokart.lcm.lidar.Vlp16LcmHandler;
+import ch.ethz.idsc.owl.gui.RenderInterface;
+import ch.ethz.idsc.owl.gui.win.GeometricLayer;
+import ch.ethz.idsc.retina.lidar.*;
+import ch.ethz.idsc.retina.lidar.vlp16.Vlp16PolarProvider;
+import ch.ethz.idsc.retina.util.StartAndStoppable;
+import ch.ethz.idsc.retina.util.math.Magnitude;
+import ch.ethz.idsc.sophus.group.Se2Utils;
+import ch.ethz.idsc.tensor.*;
+import ch.ethz.idsc.tensor.sca.Cos;
+import ch.ethz.idsc.tensor.sca.Sin;
+
+import java.awt.*;
+import java.awt.geom.Path2D;
+import java.awt.geom.Point2D;
+import java.nio.FloatBuffer;
+import java.util.*;
+
+/** class interprets sensor data from lidar */
+public class SightLineMapping implements //
+        StartAndStoppable, LidarRayBlockListener, GokartPoseListener, RenderInterface, Runnable {
+    // TODO check rationale behind constant 10000!
+    private static final int LIDAR_SAMPLES = 10000;
+    private final int SECTORS = 72;
+    private static final int HORIZON = 50; // in meters
+    // ---
+    private final LidarPolarFiringCollector lidarPolarFiringCollector = //
+            new LidarPolarFiringCollector(LIDAR_SAMPLES, 3);
+    private final Vlp16PolarProvider lidarPolarProvider = new Vlp16PolarProvider();
+    private final LidarSectorProvider lidarSectorProvider = //
+            new LidarSectorProvider(VelodyneStatics.AZIMUTH_RESOLUTION, SECTORS);
+    private final GokartPoseLcmClient gokartPoseLcmClient = new GokartPoseLcmClient();
+    private final Thread thread = new Thread(this);
+    private final Vlp16LcmHandler vlp16LcmHandler = SensorsConfig.GLOBAL.vlp16LcmHandler();
+    private final PolarObstaclePredicate predicate;
+    private final int waitMillis;
+    // ---
+    private GokartPoseEvent gokartPoseEvent;
+    private boolean isLaunched = true;
+    /** points_ferry is null or a matrix with dimension Nx3
+     * containing the cross-section of the static geometry
+     * with the horizontal plane at height of the lidar */
+    private Tensor pointsPolar_ferry = null;
+    private final Set<Tensor> pointsPolar = new TreeSet<>(Comparator.comparingDouble(point -> point.Get(0).number().doubleValue()));
+    private TreeMap<Scalar, Tensor> freeSpace = new TreeMap<>();
+
+    public SightLineMapping(PolarObstaclePredicate predicate, int waitMillis) {
+        this.predicate = predicate;
+        this.waitMillis = waitMillis;
+        // ---
+        lidarPolarProvider.setLimitLo(Magnitude.METER.toDouble(MappingConfig.GLOBAL.minDistance));
+        lidarPolarProvider.addListener(lidarPolarFiringCollector);
+        lidarSectorProvider.addListener(lidarPolarFiringCollector);
+        lidarPolarFiringCollector.addListener(this);
+        gokartPoseLcmClient.addListener(this);
+        vlp16LcmHandler.velodyneDecoder.addRayListener(lidarPolarProvider);
+        vlp16LcmHandler.velodyneDecoder.addRayListener(lidarSectorProvider);
+    }
+
+    @Override // from StartAndStoppable
+    public void start() {
+        vlp16LcmHandler.startSubscriptions();
+        gokartPoseLcmClient.startSubscriptions();
+        thread.start();
+        isLaunched = true;
+    }
+
+    @Override // from StartAndStoppable
+    public void stop() {
+        isLaunched = false;
+        thread.interrupt();
+        vlp16LcmHandler.stopSubscriptions();
+        gokartPoseLcmClient.stopSubscriptions();
+    }
+
+    @Override // from LidarRayBlockListener
+    public void lidarRayBlock(LidarRayBlockEvent lidarRayBlockEvent) {
+        if (lidarRayBlockEvent.dimensions != 3)
+            throw new RuntimeException("dim=" + lidarRayBlockEvent.dimensions);
+        // ---
+        FloatBuffer floatBuffer = lidarRayBlockEvent.floatBuffer;
+        pointsPolar_ferry = Tensors.vector(i -> Tensors.of( //
+                DoubleScalar.of(floatBuffer.get()), //
+                DoubleScalar.of(floatBuffer.get()), //
+                DoubleScalar.of(floatBuffer.get())), lidarRayBlockEvent.size());
+        thread.interrupt();
+    }
+
+    @Override // from GokartPoseListener
+    public void getEvent(GokartPoseEvent getEvent) {
+        gokartPoseEvent = getEvent;
+    }
+
+    @Override // from Runnable
+    public void run() {
+        while (isLaunched) {
+            Tensor points = pointsPolar_ferry;
+            if (Objects.nonNull(points) && Objects.nonNull(gokartPoseEvent)) {
+                freeSpace.clear();
+                for (Tensor point : points) { // point azimuth, elevation, radius
+                    Scalar azimuth = point.Get(0);
+                    if (!freeSpace.containsKey(azimuth))
+                        freeSpace.put(azimuth, Tensors.vector(azimuth.number(), 0, HORIZON));
+                    if (predicate.isObstacle(point)) {
+                        Scalar distance = point.Get(2);
+                        if (Scalars.lessThan(distance, freeSpace.get(azimuth).Get(2)))
+                            freeSpace.put(azimuth, point);
+                    }
+                }
+                synchronized (pointsPolar) {
+                    pointsPolar.addAll(freeSpace.values());
+                }
+            } else {
+                try {
+                    Thread.sleep(waitMillis);
+                } catch (Exception e) {
+                    // ---
+                }
+            }
+        }
+    }
+
+    @Override // from RenderInterface
+    public final void render(GeometricLayer geometricLayer, Graphics2D graphics) {
+        if (Objects.nonNull(gokartPoseEvent)) {
+            geometricLayer.pushMatrix(GokartPoseHelper.toSE2Matrix(gokartPoseEvent.getPose()));
+            geometricLayer.pushMatrix(Se2Utils.toSE2Matrix(SensorsConfig.GLOBAL.vlp16));
+            // ---
+            synchronized (pointsPolar) {
+                Tensor polygon = Tensor.of(pointsPolar.stream().map(point -> {
+                    Scalar azimuth = point.Get(0);
+                    Scalar elevation = point.Get(1);
+                    Scalar radius = point.Get(2);
+                    Tensor lidarCoords = Tensors.of( //
+                            radius.multiply(Cos.of(elevation)).multiply(Cos.of(azimuth)).negate(), // TODO find error causing need to negate
+                            radius.multiply(Cos.of(elevation)).multiply(Sin.of(azimuth)), //
+                            radius.multiply(Sin.of(elevation)));
+                    graphics.setColor(Color.RED);
+                    Point2D point2D = geometricLayer.toPoint2D(lidarCoords);
+                    graphics.fillRect((int) point2D.getX(), (int) point2D.getY(), 2, 2);
+                    return lidarCoords.extract(0, 2);
+                }));
+                Path2D path2D = geometricLayer.toPath2D(polygon);
+                path2D.closePath();
+                graphics.setColor(new Color(0, 255, 0, 16));
+                graphics.fill(path2D);
+                graphics.setColor(new Color(0, 255, 0, 64));
+                graphics.draw(path2D);
+                pointsPolar.clear();
+            }
+            // ---
+            geometricLayer.popMatrix();
+            geometricLayer.popMatrix();
+        }
+    }
+}
