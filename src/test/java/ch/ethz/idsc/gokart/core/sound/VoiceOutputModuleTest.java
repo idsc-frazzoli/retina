4c86aed2302cb2a774acfb9a25c5520d2a366ad3
Jan Hakenberg <jan.hakenberg@gmail.com>
2019-03-22 17:06:21 +0100

    travis user name

--- src/test/java/ch/ethz/idsc/gokart/core/sound/VoiceOutputModuleTest.java
+++ src/test/java/ch/ethz/idsc/gokart/core/sound/VoiceOutputModuleTest.java
@@ -6,17 +6,20 @@ import java.io.IOException;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.UnsupportedAudioFileException;
 
+import ch.ethz.idsc.demo.travis.TravisUserName;
 import junit.framework.TestCase;
 
 public class VoiceOutputModuleTest extends TestCase {
   public void testAll() throws InterruptedException, LineUnavailableException, IOException, UnsupportedAudioFileException {
-    VoiceOutputModule gokartVoiceOutputs = new VoiceOutputModule();
-    gokartVoiceOutputs.first();
-    for (int index = 0; index < 100; ++index)
-      for (VoiceOutput voiceOutput : VoiceOutput.values()) {
-        gokartVoiceOutputs.say(voiceOutput);
-        Thread.sleep(10);
-      }
-    gokartVoiceOutputs.last();
+    if (!TravisUserName.whoami()) {
+      VoiceOutputModule gokartVoiceOutputs = new VoiceOutputModule();
+      gokartVoiceOutputs.first();
+      for (int index = 0; index < 100; ++index)
+        for (VoiceOutput voiceOutput : VoiceOutput.values()) {
+          gokartVoiceOutputs.say(voiceOutput);
+          Thread.sleep(10);
+        }
+      gokartVoiceOutputs.last();
+    }
   }
 }
