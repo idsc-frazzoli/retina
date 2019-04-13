9bea57cb3016b94528a6d832dc538aadd2b541e3
maximilien <maximilien.picquet@gmail.com>
2019-04-13 13:45:05 +0200

    adapted structure

--- src/main/java/ch/ethz/idsc/demo/mp/pid/PIDControllerModule.java
+++ src/main/java/ch/ethz/idsc/demo/mp/pid/PIDControllerModule.java
@@ -4,10 +4,13 @@ package ch.ethz.idsc.demo.mp.pid;
 import java.util.Objects;
 import java.util.Optional;
 
+import org.bytedeco.javacpp.RealSense.intrinsics;
+
 import ch.ethz.idsc.gokart.core.pos.GokartPoseEvent;
 import ch.ethz.idsc.gokart.core.pos.GokartPoseHelper;
 import ch.ethz.idsc.gokart.core.pos.GokartPoseLcmClient;
 import ch.ethz.idsc.gokart.core.pos.GokartPoseListener;
+import ch.ethz.idsc.owl.bot.se2.pid.PIDTrajectory;
 import ch.ethz.idsc.owl.bot.se2.pid.RnCurveHelper;
 import ch.ethz.idsc.tensor.RealScalar;
 import ch.ethz.idsc.tensor.Scalar;
@@ -17,7 +20,8 @@ import ch.ethz.idsc.tensor.Tensor;
   private final GokartPoseLcmClient gokartPoseLcmClient = new GokartPoseLcmClient();
   private GokartPoseEvent gokartPoseEvent = null;
   private Optional<Tensor> optionalCurve = Optional.empty();
-  // private Scalar previousAngleError = RealScalar.ZERO;
+  private int pidIntex;
+  private PIDTrajectory previousPID;
 
   public PIDControllerModule(PIDTuningParams pidTuningParams) {
     super(pidTuningParams);
@@ -45,13 +49,20 @@ import ch.ethz.idsc.tensor.Tensor;
     if (Objects.nonNull(gokartPoseEvent) && //
         optionalCurve.isPresent() && //
         RnCurveHelper.bigEnough(optionalCurve.get())) {
-      Tensor curve = optionalCurve.get();
       Tensor poseXYphi = GokartPoseHelper.toUnitless(gokartPoseEvent.getPose());
       //
-      //
-      //
-      Scalar angleOut = RealScalar.ZERO; // TODO CHANGE
-      return Optional.of(angleOut);
+      PIDTrajectory pidTrajectory = new PIDTrajectory( //
+          pidIntex, //
+          previousPID, //
+          PIDTuningParams.GLOBAL.pidGains, //
+          optionalCurve.get(), //
+          stateTime); // TODO mcp see how to derive
+      if (PIDTuningParams.GLOBAL.clip.isInside(pidTrajectory.angleOut())) {
+        return Optional.of(pidTrajectory.angleOut());
+      }
+      this.previousPID = pidTrajectory;
+      pidIntex++;
+      return Optional.empty();
     }
     return Optional.empty();
   }
--- src/main/java/ch/ethz/idsc/demo/mp/pid/PIDTuningParams.java
+++ src/main/java/ch/ethz/idsc/demo/mp/pid/PIDTuningParams.java
@@ -1,17 +1,23 @@
 // code by mcp
 package ch.ethz.idsc.demo.mp.pid;
 
+import ch.ethz.idsc.owl.bot.se2.pid.PIDGains;
 import ch.ethz.idsc.retina.util.math.SI;
 import ch.ethz.idsc.retina.util.sys.AppResources;
+import ch.ethz.idsc.tensor.RealScalar;
 import ch.ethz.idsc.tensor.Scalar;
+import ch.ethz.idsc.tensor.opt.Pi;
 import ch.ethz.idsc.tensor.qty.Quantity;
+import ch.ethz.idsc.tensor.sca.Clip;
+import ch.ethz.idsc.tensor.sca.Clips;
 
 public class PIDTuningParams {
   public static final PIDTuningParams GLOBAL = AppResources.load(new PIDTuningParams());
   // ---
-  public Scalar pGain = Quantity.of(.1, SI.ONE);
-  public Scalar pGainPose = Quantity.of(1.0, SI.PER_METER);
-  public Scalar iGain = Quantity.of(1.0, SI.PER_SECOND);
-  public Scalar dGain = Quantity.of(10.0, SI.ONE);
+  public Scalar Kp = Quantity.of(.1, SI.PER_METER);
+  public Scalar Ki = RealScalar.ZERO;
+  public Scalar Kd = Quantity.of(10.0, "s*m^-1");
+  public PIDGains pidGains = new PIDGains(Kp, Ki, Kd);
   public final Scalar updatePeriod = Quantity.of(0.1, SI.SECOND); // 0.1[s] == 10[Hz]
+  public final Clip clip = Clips.interval(Pi.HALF.negate(), Pi.HALF); // TODO MCP check
 }
