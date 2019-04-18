9259f6cf8e80028b718be607c8ebc502859c7aab
antoniamosberger <antoniamosberger@student-net-als-2113.ethz.ch>
2019-04-18 14:30:04 +0200

    small fixes in the antilockBrakeModule

--- src/main/java/ch/ethz/idsc/demo/am/GokartLogCutter.java
+++ src/main/java/ch/ethz/idsc/demo/am/GokartLogCutter.java
@@ -1,4 +1,4 @@
-// code by jph
+//code by jph
 package ch.ethz.idsc.demo.am;
 
 import java.io.File;
@@ -6,17 +6,16 @@ import java.io.IOException;
 
 import ch.ethz.idsc.gokart.offline.gui.GokartLcmLogCutter;
 import ch.ethz.idsc.gokart.offline.gui.GokartLogFileIndexer;
-import ch.ethz.idsc.tensor.io.HomeDirectory;
 
 /* package */ enum GokartLogCutter {
   ;
   public static void main(String[] args) throws IOException {
     /** original log file */
-    File file = new File("/Users/antoniamosberger/Documents/01_6_Semester/Bachelorarbeit/20190404/20190404T154342_eb6eadfe.lcm.00");
+    File file = new File("/Users/antoniamosberger/Documents/01_6_Semester/Bachelorarbeit/20190404/20190404T143912_39258d17.lcm.00");
     /** destination folder */
     File dest = new File("/Users/antoniamosberger/Documents/01_6_Semester/Bachelorarbeit/cuts1");
     /** title of subdirectory, usually identical to log file name above */
-    String name = "20190404T154240";
+    String name = "20190404T143912";
     dest.mkdir();
     GokartLogFileIndexer gokartLogFileIndexer = GokartLogFileIndexer.create(file);
     new GokartLcmLogCutter(gokartLogFileIndexer, dest, name);
