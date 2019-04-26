154e7cfce17d5afa0d877f01cb8d2b099fdf2608
Jan Hakenberg <jan.hakenberg@gmail.com>
2019-04-26 17:12:48 +0200

    misc edits in package adas

--- src/test/java/ch/ethz/idsc/gokart/core/adas/AntilockBrakeModuleTest.java
+++ src/test/java/ch/ethz/idsc/gokart/core/adas/AntilockBrakeModuleTest.java
@@ -1,11 +1,21 @@
 // code by jph
 package ch.ethz.idsc.gokart.core.adas;
 
+import ch.ethz.idsc.tensor.RealScalar;
 import junit.framework.TestCase;
 
 public class AntilockBrakeModuleTest extends TestCase {
   public void testSimple() {
     AntilockBrakeModule antilockBrakeModule = new AntilockBrakeModule();
+    antilockBrakeModule.first();
+    assertFalse(antilockBrakeModule.putEvent().isPresent());
+    antilockBrakeModule.last();
+  }
+
+  public void testCustom() {
+    HapticSteerConfig hapticSteerConfig = new HapticSteerConfig();
+    hapticSteerConfig.absAmplitude = RealScalar.of(3);
+    AntilockBrakeModule antilockBrakeModule = new AntilockBrakeModule(hapticSteerConfig);
     antilockBrakeModule.first();
     assertFalse(antilockBrakeModule.putEvent().isPresent());
     antilockBrakeModule.last();
