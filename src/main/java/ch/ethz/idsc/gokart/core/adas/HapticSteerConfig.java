c810942750db7aa5af0fb6502ddaf1cc9754555f
antoniamosberger <antoniamosberger@student-net-hci-1715.ethz.ch>
2019-04-30 21:20:50 +0200

    - Version of PowerSteering with a third term which compensates the force caused by the lateral velocity - AntilockBrake Module: Vibration, if one of the three conditions are fulfilled.

--- src/main/java/ch/ethz/idsc/gokart/core/adas/HapticSteerConfig.java
+++ src/main/java/ch/ethz/idsc/gokart/core/adas/HapticSteerConfig.java
@@ -2,6 +2,7 @@
 package ch.ethz.idsc.gokart.core.adas;
 
 import ch.ethz.idsc.retina.util.math.NonSI;
+import ch.ethz.idsc.retina.util.math.SI;
 import ch.ethz.idsc.retina.util.sys.AppResources;
 import ch.ethz.idsc.tensor.RealScalar;
 import ch.ethz.idsc.tensor.Scalar;
@@ -16,6 +17,9 @@ public class HapticSteerConfig {
   public Scalar staticCompensation = Quantity.of(0.6, "SCT*SCE^-1");
   public Scalar dynamicCompensation = Quantity.of(90, "SCT");
   public Scalar dynamicCompensationBoundary = Quantity.of(0.1, "SCT");
+  public Scalar latForceCompensation = RealScalar.of(0.1);
+  public Scalar latForceCompensationBoundary = Quantity.of(3, SI.VELOCITY);
+  public Scalar velocityFilter = RealScalar.of(0.4);
   /** Constant Torque for Experiment */
   @FieldSubdivide(start = "-7/10[SCT]", end = "7/10[SCT]", intervals = 14)
   public Scalar constantTorque = Quantity.of(0, "SCT");
@@ -24,7 +28,7 @@ public class HapticSteerConfig {
   public Scalar vibrationFrequency = RealScalar.of(12);
   public double criticalSlip = 0.2;
   /** access value via {@link #criticalAngle()} */
-  public Scalar criticalAngleDeg = Quantity.of(12, NonSI.DEGREE_ANGLE); // die Einheit davon ist Grad, ?
+  public Scalar criticalAngleDeg = Quantity.of(12, NonSI.DEGREE_ANGLE);
   public Scalar absFrequency = RealScalar.of(5);
   public Scalar absAmplitude = RealScalar.of(0.2);
   public double absDuration = 1;
--- src/main/java/ch/ethz/idsc/gokart/gui/RunTabbedTaskGui.java
+++ src/main/java/ch/ethz/idsc/gokart/gui/RunTabbedTaskGui.java
@@ -16,8 +16,10 @@ import ch.ethz.idsc.demo.mg.slam.online.SEyeSlamVisualModule;
 import ch.ethz.idsc.demo.mp.pid.PIDModule;
 import ch.ethz.idsc.demo.mp.pid.PIDTestTrackModule;
 import ch.ethz.idsc.gokart.core.AutoboxSocketModule;
+import ch.ethz.idsc.gokart.core.adas.AntilockBrakeCheckConditions;
 import ch.ethz.idsc.gokart.core.adas.AntilockBrakeModule;
 import ch.ethz.idsc.gokart.core.adas.PowerSteeringModule;
+import ch.ethz.idsc.gokart.core.adas.PowerSteeringModuleVersion2;
 import ch.ethz.idsc.gokart.core.adas.SteeringExperimentModule;
 import ch.ethz.idsc.gokart.core.fuse.LinmotCoolingModule;
 import ch.ethz.idsc.gokart.core.fuse.LinmotSafetyModule;
@@ -117,7 +119,9 @@ enum RunTabbedTaskGui {
       ImprovedNormalizedTorqueVectoringManualModule.class, //
       LookupTableRimoThrustManualModule.class, //
       PowerSteeringModule.class, //
+      PowerSteeringModuleVersion2.class, //
       SteeringExperimentModule.class, //
+      AntilockBrakeCheckConditions.class, //
       AntilockBrakeModule.class //
   );
   static final List<Class<? extends AbstractModule>> MODULES_AUT = Arrays.asList( //
