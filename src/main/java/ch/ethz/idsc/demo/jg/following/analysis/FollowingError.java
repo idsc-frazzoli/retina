3fb2013f26524c84a3a2238585c6ab82d56c8dcf
JoelGaechter <joelgaechter@bluewin.ch>
2019-05-08 14:09:13 +0200

    following error split

---
+++ src/main/java/ch/ethz/idsc/demo/jg/following/analysis/FollowingError.java
@@ -0,0 +1,66 @@
+// code by gjoel
+package ch.ethz.idsc.demo.jg.following.analysis;
+
+import java.util.Optional;
+
+import ch.ethz.idsc.owl.math.planar.Extract2D;
+import ch.ethz.idsc.retina.util.math.SI;
+import ch.ethz.idsc.tensor.DoubleScalar;
+import ch.ethz.idsc.tensor.Scalar;
+import ch.ethz.idsc.tensor.Tensor;
+import ch.ethz.idsc.tensor.Tensors;
+import ch.ethz.idsc.tensor.qty.Quantity;
+import ch.ethz.idsc.tensor.red.Max;
+import ch.ethz.idsc.tensor.red.Mean;
+import ch.ethz.idsc.tensor.red.Min;
+import ch.ethz.idsc.tensor.red.Norm;
+import ch.ethz.idsc.tensor.sca.Round;
+
+public class FollowingError {
+  private Tensor reference = Tensors.empty();
+  private Tensor errors = Tensors.empty();
+  // ---
+  private Tensor times = Tensors.of( //
+      Quantity.of(DoubleScalar.POSITIVE_INFINITY, SI.SECOND), //
+      Quantity.of(DoubleScalar.NEGATIVE_INFINITY, SI.SECOND));
+
+  /** @param reference curve or trajectory */
+  protected void setReference(Tensor reference) {
+    this.reference = Tensor.of(reference.stream().map(Extract2D.FUNCTION)).unmodifiable();
+  }
+
+  protected Tensor getReference() {
+    return reference.unmodifiable();
+  }
+
+  /** @param time [s]
+   * @param pose of vehicle {x[m], y[m], angle} */
+  protected void insert(Scalar time, Tensor pose) {
+    times.set(Min.of(times.Get(0), time), 0);
+    times.set(Max.of(times.Get(1), time), 1);
+    error(pose).ifPresent(errors::append);
+  }
+
+  /** @param pose of vehicle
+   * @return approximation of error by distance to closest reference point */
+  private Optional<Scalar> error(Tensor pose) {
+    Tensor pose2D = Extract2D.FUNCTION.apply(pose);
+    Tensor distances = Tensor.of(reference.stream().map(tensor -> tensor.subtract(pose2D)).map(Norm._2::ofVector));
+    return distances.stream().reduce(Min::of).map(Tensor::Get);
+  }
+
+  public Scalar averageError() {
+    return Mean.of(errors).Get();
+  }
+
+  public Scalar accumulatedError() {
+    return errors.stream().map(Scalar.class::cast).reduce(Scalar::add).get();
+  }
+
+  public String getReport() {
+    return "following error (" + this.getClass().getSimpleName() + ")\n" + //
+        "\ttime:\t" + times.Get(0).map(Round._2) + " - " + times.Get(1).map(Round._2) + "\n" + //
+        "\taverage error:\t" + averageError().map(Round._4) + "\n" + //
+        "\taccumulated error:\t" + accumulatedError().map(Round._4);
+  }
+}
