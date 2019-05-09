459ccd2ffe97d398affc29d8ef263e252b1710d9
Jan Hakenberg <jan.hakenberg@gmail.com>
2019-05-09 07:33:58 +0200

    steering turning ratio

--- src/main/java/ch/ethz/idsc/demo/GokartLogFile.java
+++ src/main/java/ch/ethz/idsc/demo/GokartLogFile.java
@@ -1821,6 +1821,10 @@ public enum GokartLogFile implements LogFile {
   /** demo trajectory planning
    * data collection for steering mapping */
   _20190507T094525_c817d5db,
+  /** fast manual driving */
+  _20190507T122145_06a2a5f4,
+  /** mpc */
+  _20190507T142359_06a2a5f4,
   /***************************************************/
   ;
   // ---
