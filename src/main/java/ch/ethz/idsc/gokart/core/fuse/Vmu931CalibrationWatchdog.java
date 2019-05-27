8a0e87023a609bdeaadf5e9e0822222e0985c1de
Jan Hakenberg <jan.hakenberg@gmail.com>
2019-05-27 14:55:50 +0200

    vmu931 calibration watchdog

--- src/main/java/ch/ethz/idsc/gokart/core/fuse/Vmu931CalibrationWatchdog.java
+++ src/main/java/ch/ethz/idsc/gokart/core/fuse/Vmu931CalibrationWatchdog.java
@@ -3,7 +3,6 @@ package ch.ethz.idsc.gokart.core.fuse;
 
 import java.util.Objects;
 import java.util.Optional;
-import java.util.function.Supplier;
 
 import ch.ethz.idsc.gokart.dev.rimo.RimoPutEvent;
 import ch.ethz.idsc.gokart.dev.rimo.RimoSocket;
@@ -22,19 +21,12 @@ public class Vmu931CalibrationWatchdog extends EmergencyModule<RimoPutEvent> imp
   // ---
   private final Vmu931ImuLcmClient vmu931ImuLcmClient = new Vmu931ImuLcmClient();
   private final Watchdog watchdog = SoftWatchdog.barking(TIMEOUT_S);
-  /** status provider whether device is calibrated */
-  private final Supplier<Boolean> isCalibrated;
 
-  public Vmu931CalibrationWatchdog() {
+  private static boolean isCalibrated() {
     Vmu931LcmServerModule vmu931LcmServerModule = //
         ModuleAuto.INSTANCE.getInstance(Vmu931LcmServerModule.class);
-    isCalibrated = Objects.isNull(vmu931LcmServerModule) // is vmu931 device absent
-        ? () -> false // never drive without vmu931
-        : vmu931LcmServerModule::isCalibrated; // only drive when device is calibrated
-  }
-
-  /* package */ Vmu931CalibrationWatchdog(Supplier<Boolean> isCalibrated) {
-    this.isCalibrated = isCalibrated;
+    return Objects.nonNull(vmu931LcmServerModule) //
+        && vmu931LcmServerModule.isCalibrated();
   }
 
   @Override // from AbstractModule
@@ -52,7 +44,7 @@ public class Vmu931CalibrationWatchdog extends EmergencyModule<RimoPutEvent> imp
 
   @Override // from PutProvider
   public Optional<RimoPutEvent> putEvent() {
-    return isCalibrated.get() && !watchdog.isBarking() // calibrated and active
+    return isCalibrated() && !watchdog.isBarking() // calibrated and active
         ? Optional.empty()
         : RimoPutEvent.OPTIONAL_RIMO_PASSIVE;
   }
