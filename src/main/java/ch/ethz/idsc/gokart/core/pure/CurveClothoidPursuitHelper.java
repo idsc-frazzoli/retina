a682156c08de826983e3f0c06339980473800850
JoelGaechter <joelgaechter@bluewin.ch>
2019-05-27 11:22:14 +0200

    more precise clothoid length

--- src/main/java/ch/ethz/idsc/gokart/core/pure/CurveClothoidPursuitHelper.java
+++ src/main/java/ch/ethz/idsc/gokart/core/pure/CurveClothoidPursuitHelper.java
@@ -5,10 +5,8 @@ import java.util.List;
 import java.util.function.Predicate;
 
 import ch.ethz.idsc.owl.bot.se2.glc.DynamicRatioLimit;
-import ch.ethz.idsc.owl.math.planar.Extract2D;
 import ch.ethz.idsc.tensor.Scalar;
 import ch.ethz.idsc.tensor.Tensor;
-import ch.ethz.idsc.tensor.red.Norm;
 
 // TODO JPH rename
 /* package */ enum CurveClothoidPursuitHelper {
@@ -28,15 +26,5 @@ import ch.ethz.idsc.tensor.red.Norm;
     return ratio -> ratioLimits.stream() //
         .map(dynamicRatioLimit -> dynamicRatioLimit.at(pose, speed)) //
         .allMatch(dynamicRatioLimit -> dynamicRatioLimit.isInside(ratio));
-  }
-
-  /** @param curve geodesic
-   * @return approximated length of curve */
-  public static Scalar curveLength(Tensor curve) {
-    Tensor curve_ = Tensor.of(curve.stream().map(Extract2D.FUNCTION));
-    int n = curve_.length();
-    return curve_.extract(1, n).subtract(curve_.extract(0, n - 1)).stream() //
-        .map(Norm._2::ofVector) //
-        .reduce(Scalar::add).get();
   }
 }
