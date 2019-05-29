51e8f29676b908cd66a3993a954ea7e8f82313db
JoelGaechter <joelgaechter@bluewin.ch>
2019-05-29 11:25:49 +0200

    clothoid plan test

--- src/test/java/ch/ethz/idsc/gokart/core/pure/ClothoidPlanTest.java
+++ src/test/java/ch/ethz/idsc/gokart/core/pure/ClothoidPlanTest.java
@@ -5,25 +5,31 @@ import java.util.Optional;
 
 import ch.ethz.idsc.retina.util.math.Magnitude;
 import ch.ethz.idsc.retina.util.pose.PoseHelper;
+import ch.ethz.idsc.sophus.group.Se2GroupElement;
+import ch.ethz.idsc.tensor.Tensor;
 import ch.ethz.idsc.tensor.Tensors;
 import ch.ethz.idsc.tensor.sca.Clips;
 import junit.framework.TestCase;
 
 public class ClothoidPlanTest extends TestCase {
   public void testForward() {
-    Optional<ClothoidPlan> optional = ClothoidPlan.from( //
-        PoseHelper.attachUnits(Tensors.vector(1, 2, 3)), //
-        PoseHelper.attachUnits(Tensors.vector(-1, 3, 3)), true);
+    Tensor pose = PoseHelper.attachUnits(Tensors.vector(-1, 3, 3));
+    Tensor lookAhead = PoseHelper.attachUnits(Tensors.vector(1, 2, 3));
+    Optional<ClothoidPlan> optional = ClothoidPlan.from(lookAhead, pose, true);
     ClothoidPlan clothoidPlan = optional.get();
     Clips.interval(0.07, 0.09).requireInside(Magnitude.PER_METER.apply(clothoidPlan.ratio()));
+    Tensor goal = new Se2GroupElement(pose).combine(lookAhead);
+    assertEquals(goal, clothoidPlan.curve().get(clothoidPlan.curve().length() - 1));
   }
 
   public void testReverse() {
-    Optional<ClothoidPlan> optional = ClothoidPlan.from( //
-        PoseHelper.attachUnits(Tensors.vector(1, 2, 3)), //
-        PoseHelper.attachUnits(Tensors.vector(10, 500, 2)), false);
+    Tensor pose = PoseHelper.attachUnits(Tensors.vector(10, 500, 2));
+    Tensor lookAhead = PoseHelper.attachUnits(Tensors.vector(1, 2, 3)); // imagine this being seen through rear mirror
+    Optional<ClothoidPlan> optional = ClothoidPlan.from(lookAhead, pose, false);
     ClothoidPlan clothoidPlan = optional.get();
-    // TODO JG strange values
     Clips.interval(0.07, 0.09).requireInside(Magnitude.PER_METER.apply(clothoidPlan.ratio()));
+    CurveClothoidPursuitHelper.mirrorAndReverse(lookAhead);
+    Tensor goal = new Se2GroupElement(pose).combine(lookAhead);
+    assertEquals(goal, clothoidPlan.curve().get(clothoidPlan.curve().length() - 1));
   }
 }
