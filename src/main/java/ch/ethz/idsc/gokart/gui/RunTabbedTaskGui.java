74675e27028e7460dd6b13f1a540cb38b86c472b
antoniamosberger <antoniamosberger@student-net-cx-0509.ethz.ch>
2019-06-12 14:34:34 +0200

    forgot GUI change

--- src/main/java/ch/ethz/idsc/gokart/gui/RunTabbedTaskGui.java
+++ src/main/java/ch/ethz/idsc/gokart/gui/RunTabbedTaskGui.java
@@ -18,11 +18,11 @@ import ch.ethz.idsc.demo.mp.pid.PIDTestTrackModule;
 import ch.ethz.idsc.gokart.core.AutoboxSocketModule;
 import ch.ethz.idsc.gokart.core.adas.AntilockBrakeCheckConditions;
 import ch.ethz.idsc.gokart.core.adas.AntilockBrakeModule;
-import ch.ethz.idsc.gokart.core.adas.AntilockBrakeTempExperimentModule;
 import ch.ethz.idsc.gokart.core.adas.AntilockBrakeV2Module;
 import ch.ethz.idsc.gokart.core.adas.AntilockBrakeV3Module;
 import ch.ethz.idsc.gokart.core.adas.NoFrictionExperiment;
 import ch.ethz.idsc.gokart.core.adas.PowerSteeringModule;
+import ch.ethz.idsc.gokart.core.adas.SetVelNotsmartBrakingModule;
 import ch.ethz.idsc.gokart.core.adas.SpeedLimitPerSectionModule;
 import ch.ethz.idsc.gokart.core.adas.SteerVibrationModule;
 import ch.ethz.idsc.gokart.core.adas.SteeringExperimentModule;
@@ -133,7 +133,7 @@ import ch.ethz.idsc.tensor.io.ResourceData;
       AntilockBrakeModule.class, //
       AntilockBrakeV2Module.class, //
       AntilockBrakeV3Module.class, //
-      AntilockBrakeTempExperimentModule.class, //
+      SetVelNotsmartBrakingModule.class, //
       SpeedLimitPerSectionModule.class, //
       AutomaticPowerTestModule.class, //
       RimoThrustManualModule.class, //
