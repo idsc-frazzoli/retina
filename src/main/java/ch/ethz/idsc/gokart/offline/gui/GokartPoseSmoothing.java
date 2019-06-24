9aa1c21e432a6b434bddb89ce04eca19b24d4674
Jan Hakenberg <jan.hakenberg@gmail.com>
2019-06-24 15:33:03 +0200

    minor edits

--- src/main/java/ch/ethz/idsc/gokart/offline/gui/GokartPoseSmoothing.java
+++ src/main/java/ch/ethz/idsc/gokart/offline/gui/GokartPoseSmoothing.java
@@ -4,7 +4,6 @@ package ch.ethz.idsc.gokart.offline.gui;
 import ch.ethz.idsc.sophus.filter.CenterFilter;
 import ch.ethz.idsc.sophus.filter.ga.GeodesicCenter;
 import ch.ethz.idsc.sophus.lie.se2.Se2Geodesic;
-import ch.ethz.idsc.sophus.math.win.WindowCenterSampler;
 import ch.ethz.idsc.tensor.Tensor;
 import ch.ethz.idsc.tensor.opt.TensorUnaryOperator;
 import ch.ethz.idsc.tensor.sca.win.GaussianWindow;
@@ -14,7 +13,7 @@ public enum GokartPoseSmoothing implements TensorUnaryOperator {
   // ---
   private static final int HALF_WIDTH = 6;
   private static final TensorUnaryOperator TENSOR_UNARY_OPERATOR = //
-      GeodesicCenter.of(Se2Geodesic.INSTANCE, WindowCenterSampler.of(GaussianWindow.FUNCTION));
+      GeodesicCenter.of(Se2Geodesic.INSTANCE, GaussianWindow.FUNCTION);
 
   @Override // from TensorUnaryOperator
   public Tensor apply(Tensor tensor) {
