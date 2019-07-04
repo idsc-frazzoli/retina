cb1cf7383cf29b7b71ee7ed601d0ddf52b90ae01
mcp10 <maximilien.picquet@gmail.com>
2019-07-04 12:23:12 +0200

    updated analysis

--- src/main/java/ch/ethz/idsc/demo/mp/PacejkaAnalysis.java
+++ src/main/java/ch/ethz/idsc/demo/mp/PacejkaAnalysis.java
@@ -7,13 +7,19 @@ import java.nio.ByteBuffer;
 import java.util.Objects;
 
 import ch.ethz.idsc.gokart.core.pos.GokartPoseEvent;
+import ch.ethz.idsc.gokart.dev.rimo.RimoGetEvent;
+import ch.ethz.idsc.gokart.dev.steer.SteerColumnTracker;
+import ch.ethz.idsc.gokart.dev.steer.SteerGetEvent;
 import ch.ethz.idsc.gokart.gui.GokartLcmChannel;
 import ch.ethz.idsc.gokart.gui.top.SensorsConfig;
 import ch.ethz.idsc.gokart.lcm.OfflineLogPlayer;
+import ch.ethz.idsc.gokart.lcm.autobox.RimoLcmServer;
+import ch.ethz.idsc.gokart.lcm.autobox.SteerLcmServer;
 import ch.ethz.idsc.gokart.offline.api.OfflineTableSupplier;
 import ch.ethz.idsc.gokart.offline.channel.Vmu931ImuChannel;
 import ch.ethz.idsc.retina.imu.vmu931.Vmu931ImuFrame;
 import ch.ethz.idsc.retina.util.math.Magnitude;
+import ch.ethz.idsc.retina.util.math.SI;
 import ch.ethz.idsc.retina.util.pose.VelocityHelper;
 import ch.ethz.idsc.tensor.RealScalar;
 import ch.ethz.idsc.tensor.Scalar;
@@ -22,12 +28,17 @@ import ch.ethz.idsc.tensor.io.CsvFormat;
 import ch.ethz.idsc.tensor.io.Export;
 import ch.ethz.idsc.tensor.io.HomeDirectory;
 import ch.ethz.idsc.tensor.io.TableBuilder;
+import ch.ethz.idsc.tensor.qty.Quantity;
 import ch.ethz.idsc.tensor.sca.Round;
 
 /** export for UKF/EKF used in Parameter identification in Pacejka */
 /* package */ class PacejkaAnalysis implements OfflineTableSupplier {
   private final TableBuilder tableBuilder = new TableBuilder();
   private GokartPoseEvent gokartPoseEvent;
+  private Scalar wheelAngularSpeedL = Quantity.of(0, SI.PER_SECOND);
+  private Scalar wheelAngularSpeedR = Quantity.of(0, SI.PER_SECOND);
+  private Scalar steerPosition = Quantity.of(0, "SCE");
+  private final SteerColumnTracker steerColumnTracker = new SteerColumnTracker();
 
   @Override // from OfflineLogListener
   public void event(Scalar time, String channel, ByteBuffer byteBuffer) {
@@ -38,12 +49,26 @@ import ch.ethz.idsc.tensor.sca.Round;
             time.map(Magnitude.SECOND), // [1]
             RealScalar.of(vmu931ImuFrame.timestamp_ms()), // [2]
             VelocityHelper.toUnitless(gokartPoseEvent.getVelocity()).map(Round._5), // [3][4][5]
-            SensorsConfig.GLOBAL.getPlanarVmu931Imu().accXY(vmu931ImuFrame).map(Magnitude.ACCELERATION).map(Round._5) // [6][7]
-        );
+            SensorsConfig.GLOBAL.getPlanarVmu931Imu().acceleration(vmu931ImuFrame).map(Magnitude.ACCELERATION).map(Round._5), // [6][7][8]
+            wheelAngularSpeedL.map(Magnitude.PER_SECOND), // [9]
+            wheelAngularSpeedR.map(Magnitude.PER_SECOND), // [10]
+            RealScalar.of(steerPosition.number().floatValue()));// [11] //in radiants
       }
     } else //
     if (channel.equals(GokartLcmChannel.POSE_LIDAR)) {
       gokartPoseEvent = GokartPoseEvent.of(byteBuffer);
+    } else //
+    if (channel.equals(RimoLcmServer.CHANNEL_GET)) {
+      RimoGetEvent rge = new RimoGetEvent(byteBuffer);
+      wheelAngularSpeedL = rge.getTireL.getAngularRate_Y();
+      wheelAngularSpeedR = rge.getTireR.getAngularRate_Y();
+    } else //
+    if (channel.equals(SteerLcmServer.CHANNEL_GET)) {
+      SteerGetEvent sge = new SteerGetEvent(byteBuffer);
+      steerColumnTracker.getEvent(sge);
+      if (steerColumnTracker.isCalibratedAndHealthy()) {
+        steerPosition = steerColumnTracker.getSteerColumnEncoderCentered();
+      }
     }
   }
 
