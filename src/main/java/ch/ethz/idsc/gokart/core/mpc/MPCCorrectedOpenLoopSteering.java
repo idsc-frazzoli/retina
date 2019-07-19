5d437f2c6521c9bea49110719a1ec68f9ff31814
Jan Hakenberg <jan.hakenberg@gmail.com>
2019-07-19 13:37:10 +0200

    mpc state provider client obsolete

--- src/main/java/ch/ethz/idsc/gokart/core/mpc/MPCCorrectedOpenLoopSteering.java
+++ src/main/java/ch/ethz/idsc/gokart/core/mpc/MPCCorrectedOpenLoopSteering.java
@@ -8,39 +8,22 @@ import ch.ethz.idsc.tensor.Scalar;
 import ch.ethz.idsc.tensor.Tensor;
 import ch.ethz.idsc.tensor.Tensors;
 
+// TODO JPH extend from MPCOpenLoopSteering
 /* package */ class MPCCorrectedOpenLoopSteering extends MPCSteering {
-  // private final MPCActiveCompensationLearning mpcActiveCompensationLearning = MPCActiveCompensationLearning.getInstance();
-  private final MPCOptimizationConfig config = MPCOptimizationConfig.GLOBAL;
-  // TODO MH variable not used:
-  private MPCStateEstimationProvider mpcStateProvider;
+  private final MPCOptimizationConfig mpcOptimizationConfig = MPCOptimizationConfig.GLOBAL;
 
-  @Override
+  @Override // from MPCSteering
   Optional<Tensor> getSteering(Scalar time) {
-    Scalar dotFactor = MPCOptimizationConfig.GLOBAL.steerDamp;
-    Scalar controlTime = time.add(config.steerAntiLag);
+    Scalar controlTime = time.add(mpcOptimizationConfig.steerAntiLag);
     ControlAndPredictionStep cnpStep = getStep(controlTime);
     if (Objects.isNull(cnpStep))
       return Optional.empty();
     Scalar timeSinceLastStep = getTimeSinceLastStep(controlTime);
     Scalar rampUp = timeSinceLastStep.multiply(cnpStep.gokartControl().getudotS());
+    Scalar dotFactor = mpcOptimizationConfig.steerDamp;
     return Optional.of(Tensors.of( //
         cnpStep.gokartState().getS().add(rampUp), //
         cnpStep.gokartControl().getudotS().multiply(dotFactor))//
-        .multiply(MPCOptimizationConfig.GLOBAL.steerMultiplicator));
-  }
-
-  // TODO MH remove commented out code below:
-  // @Override
-  // public Scalar getDotSteering(Scalar time) {
-  // Scalar controlTime = time.add(config.steerAntiLag);
-  // ControlAndPredictionStep cnpStep = getStep(controlTime);
-  // if (Objects.isNull(cnpStep))
-  // return null;
-  // return
-  // .multiply(mpcActiveCompensationLearning.steeringCorrection);
-  // }
-  @Override
-  public void setStateEstimationProvider(MPCStateEstimationProvider mpcstateProvider) {
-    this.mpcStateProvider = mpcstateProvider;
+        .multiply(mpcOptimizationConfig.steerMultiplicator));
   }
 }
