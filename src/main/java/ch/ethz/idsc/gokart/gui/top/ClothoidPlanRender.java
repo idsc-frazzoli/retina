714419585669827cea7a7b032de5350040b65568
Jan Hakenberg <jan.hakenberg@gmail.com>
2019-08-03 02:29:25 +0200

    head tail interface, sophus util plot

--- src/main/java/ch/ethz/idsc/gokart/gui/top/ClothoidPlanRender.java
+++ src/main/java/ch/ethz/idsc/gokart/gui/top/ClothoidPlanRender.java
@@ -6,7 +6,7 @@ import java.awt.Color;
 import ch.ethz.idsc.gokart.core.pure.ClothoidPlan;
 import ch.ethz.idsc.gokart.core.pure.ClothoidPlanListener;
 import ch.ethz.idsc.sophus.app.api.PathRender;
-import ch.ethz.idsc.sophus.crv.clothoid.ClothoidTerminalRatios;
+import ch.ethz.idsc.sophus.crv.clothoid.Clothoid3;
 import ch.ethz.idsc.tensor.red.Nest;
 
 public class ClothoidPlanRender extends PathRender implements ClothoidPlanListener {
@@ -19,6 +19,6 @@ public class ClothoidPlanRender extends PathRender implements ClothoidPlanListen
   @Override // from ClothoidPlanListener
   public void planReceived(ClothoidPlan clothoidPlan) {
     // TODO GJOEL curve is already refined
-    setCurve(Nest.of(ClothoidTerminalRatios.CURVE_SUBDIVISION::string, clothoidPlan.curve(), REFINEMENT), false);
+    setCurve(Nest.of(Clothoid3.CURVE_SUBDIVISION::string, clothoidPlan.curve(), REFINEMENT), false);
   }
 }
