1bba820ea0424633bf7c1c34d4544ec9caf5145e
Jan Hakenberg <jan.hakenberg@gmail.com>
2019-08-09 09:26:22 +0200

    path progress image

--- src/main/java/ch/ethz/idsc/gokart/core/track/BSplineTrackDemo.java
+++ src/main/java/ch/ethz/idsc/gokart/core/track/BSplineTrackDemo.java
@@ -3,12 +3,17 @@ package ch.ethz.idsc.gokart.core.track;
 
 import java.awt.BasicStroke;
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Graphics2D;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.geom.Path2D;
+import java.awt.image.BufferedImage;
 
 import javax.swing.JToggleButton;
 
 import ch.ethz.idsc.owl.gui.RenderInterface;
+import ch.ethz.idsc.owl.gui.ren.AxesRender;
 import ch.ethz.idsc.owl.gui.win.GeometricLayer;
 import ch.ethz.idsc.retina.util.math.Magnitude;
 import ch.ethz.idsc.retina.util.math.SI;
@@ -17,30 +22,97 @@ import ch.ethz.idsc.sophus.app.api.ControlPointsDemo;
 import ch.ethz.idsc.sophus.app.api.GeodesicDisplays;
 import ch.ethz.idsc.sophus.lie.se2.Se2Matrix;
 import ch.ethz.idsc.tensor.RealScalar;
+import ch.ethz.idsc.tensor.Scalar;
 import ch.ethz.idsc.tensor.Tensor;
+import ch.ethz.idsc.tensor.Tensors;
+import ch.ethz.idsc.tensor.alg.Subdivide;
+import ch.ethz.idsc.tensor.img.ArrayPlot;
+import ch.ethz.idsc.tensor.img.ColorDataGradient;
+import ch.ethz.idsc.tensor.img.ColorDataGradients;
+import ch.ethz.idsc.tensor.img.ColorFormat;
+import ch.ethz.idsc.tensor.img.Hue;
+import ch.ethz.idsc.tensor.io.ImageFormat;
 import ch.ethz.idsc.tensor.lie.CirclePoints;
+import ch.ethz.idsc.tensor.mat.Inverse;
+import ch.ethz.idsc.tensor.opt.ScalarTensorFunction;
 import ch.ethz.idsc.tensor.qty.Quantity;
 import ch.ethz.idsc.tensor.sca.Ramp;
 
-public class BSplineTrackDemo extends ControlPointsDemo {
+/* package */ class BSplineTrackDemo extends ControlPointsDemo {
   private static final Tensor CIRCLE = CirclePoints.of(27);
+  private static final ColorDataGradient COLOR_DATA_GRADIENT_STRING = //
+      ColorDataGradients.CLASSIC.deriveWithOpacity(RealScalar.of(0.5));
   // ---
   private final JToggleButton jToggleView = new JToggleButton("view");
-  final JToggleButton jToggleClosed = new JToggleButton("closed");
+  /* package */ final JToggleButton jToggleClosed = new JToggleButton("closed");
+  private final JToggleButton jButton = new JToggleButton("render");
+  private final ColorDataGradient colorDataGradient;
 
   public BSplineTrackDemo() {
     super(true, GeodesicDisplays.R2_ONLY);
     timerFrame.jToolBar.add(jToggleView);
     timerFrame.jToolBar.add(jToggleClosed);
+    timerFrame.jToolBar.add(jButton);
+    jButton.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed(ActionEvent e) {
+        timerFrame.geometricComponent.getModel2Pixel();
+      }
+    });
+    timerFrame.geometricComponent.addRenderInterfaceBackground(AxesRender.INSTANCE);
     jToggleView.addActionListener(e -> setPositioningEnabled(!jToggleView.isSelected()));
+    ScalarTensorFunction scalarTensorFunction = //
+        value -> ColorFormat.toVector(Hue.of(value.number().doubleValue(), 0.7, 1, 0.5));
+    Tensor tensor = Subdivide.of(0, 1, 255).map(scalarTensorFunction);
+    for (int index = 0; index < tensor.length(); index += 16)
+      tensor.set(Tensors.vector(0, 0, 0, 0), index);
+    colorDataGradient = new ColorDataGradient() {
+      @Override
+      public Tensor apply(Scalar t) {
+        return tensor.get((int) (t.number().doubleValue() * 255));
+      }
+
+      @Override
+      public ColorDataGradient deriveWithOpacity(Scalar opacity) {
+        throw new UnsupportedOperationException();
+      }
+    };
   }
 
   @Override
   public void render(GeometricLayer geometricLayer, Graphics2D graphics) {
-    Tensor points_xyr = getControlPointsSe2().copy();
-    points_xyr.set(Ramp.FUNCTION, Tensor.ALL, 2);
+    final Tensor points_xya = getControlPointsSe2().copy();
+    points_xya.set(Ramp.FUNCTION, Tensor.ALL, 2);
+    if (1 < points_xya.length() && jButton.isSelected()) {
+      Tensor points_xyr = points_xya.map(s -> Quantity.of(s, SI.METER));
+      BSplineTrack bSplineTrack = jToggleClosed.isSelected() //
+          ? new CyclicBSplineTrack(points_xyr)
+          : new StringBSplineTrack(points_xyr);
+      Dimension dimension = timerFrame.geometricComponent.jComponent.getSize();
+      Tensor pixel2model = Inverse.of(timerFrame.geometricComponent.getModel2Pixel());
+      GeometricLayer gl = GeometricLayer.of(pixel2model);
+      int step = 5;
+      Tensor raster = Tensors.reserve(dimension.height / step);
+      for (int y = 0; y < dimension.height; y += step) {
+        Tensor row = Tensors.reserve(dimension.width / step);
+        for (int x = 0; x < dimension.width; x += step) {
+          Tensor vector = gl.toVector(x, y);
+          Scalar progress = bSplineTrack.getNearestPathProgress(vector);
+          row.append(progress);
+        }
+        raster.append(row);
+      }
+      ColorDataGradient colorDataGradient = jToggleClosed.isSelected() //
+          ? this.colorDataGradient
+          : COLOR_DATA_GRADIENT_STRING;
+      Tensor tensor = ArrayPlot.of(raster, colorDataGradient);
+      BufferedImage bufferedImage = ImageFormat.of(tensor);
+      graphics.drawImage(bufferedImage, 0, 0, //
+          bufferedImage.getWidth() * step, //
+          bufferedImage.getHeight() * step, null);
+    }
     {
-      for (Tensor point : points_xyr) {
+      for (Tensor point : points_xya) {
         geometricLayer.pushMatrix(Se2Matrix.translation(point));
         Path2D path2d = geometricLayer.toPath2D(CIRCLE.multiply(point.Get(2)));
         path2d.closePath();
@@ -51,8 +123,8 @@ public class BSplineTrackDemo extends ControlPointsDemo {
       }
     }
     renderControlPoints(geometricLayer, graphics);
-    if (1 < points_xyr.length()) {
-      points_xyr = points_xyr.map(s -> Quantity.of(s, SI.METER));
+    if (1 < points_xya.length()) {
+      Tensor points_xyr = points_xya.map(s -> Quantity.of(s, SI.METER));
       BSplineTrack bSplineTrack = jToggleClosed.isSelected() //
           ? new CyclicBSplineTrack(points_xyr)
           : new StringBSplineTrack(points_xyr);
