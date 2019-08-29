4bd27e41fcd275ef3d1b40559f30a92d47ca3f84
MvBue <vbueremi@student.ethz.ch>
2019-08-29 17:21:47 +0200

    added linear NN

--- src_MATLAB/MPCGokart/ForcesMPCPathFollowingDynamic_NN/interstagedx.m
+++ src_MATLAB/MPCGokart/ForcesMPCPathFollowingDynamic_NN/interstagedx.m
@@ -48,7 +48,8 @@ function dx = interstagedx(x,u,p)
    
     %(VELX,VELY,VELROTZ,BETA,AB,TV, param)
     %[ACCX,ACCY,ACCROTZ,frontabcorr] = modelDx(vx,vy,dottheta,ackermannAngle,ab,tv, param);
-    [ACCX,ACCY,ACCROTZ] = modelDx_NN_11(vx,vy,dottheta,ackermannAngle,ab,tv, param);
+    %[ACCX,ACCY,ACCROTZ] = modelDx_NN_11(vx,vy,dottheta,ackermannAngle,ab,tv, param);
+    [ACCX,ACCY,ACCROTZ] = modelDx_NN_linear(vx,vy,dottheta,ackermannAngle,ab,tv, param);
     
     
     import casadi.*
