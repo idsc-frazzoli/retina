10ccc693c24e6569877cfced48de87152ab5cc17
JoelGaechter <joelgaechter@bluewin.ch>
2020-02-03 11:44:28 +0100

    led corrections

--- src/main/java/ch/ethz/idsc/gokart/lcm/led/LEDLcm.java
+++ src/main/java/ch/ethz/idsc/gokart/lcm/led/LEDLcm.java
@@ -3,7 +3,6 @@ package ch.ethz.idsc.gokart.lcm.led;
 
 import java.awt.Color;
 import java.nio.ByteBuffer;
-import java.util.Arrays;
 
 import ch.ethz.idsc.gokart.dev.led.LEDStatus;
 import ch.ethz.idsc.gokart.lcm.ArrayFloatBlob;
@@ -27,17 +26,8 @@ public class LEDLcm {
 
   /** decode gets back {@link LEDStatus} from a ByteBuffer using ArrayFloatBlob::decode. */
   public static LEDStatus decode(ByteBuffer byteBuffer) {
-    Tensor decoded = VectorQ.require(ArrayFloatBlob.decode(byteBuffer));
+    Tensor decoded = VectorQ.requireLength(ArrayFloatBlob.decode(byteBuffer), LEDStatus.LENGTH);
     int[] array = decoded.stream().mapToInt(t -> t.Get().number().intValue()).toArray();
-    switch (array.length) {
-    case 1:
-      return new LEDStatus(array[0]);
-    case 2:
-      return new LEDStatus(array[0], array[1]);
-    case 5:
-      return new LEDStatus(array[0], array[1], new Color(array[2], array[3], array[4]));
-    default:
-      throw new RuntimeException("expected a message of length 1, 2 or 5, got " + array.length + ": " + Arrays.toString(array));
-    }
+    return new LEDStatus(array[0], array[1], new Color(array[2], array[3], array[4]));
   }
 }
